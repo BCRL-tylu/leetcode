b0314f00da787aaae43ec6bd6e879340f66dee4e
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-11-27 12:05:24 +1100

    Time: 24 ms (91.19%) | Memory: 66.9 MB (80.98%) - LeetSync

---
+++ 3517-shortest-distance-after-road-addition-queries-i/shortest-distance-after-road-addition-queries-i.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
+        // Use vector instead of unordered_map
+        vector<pair<int, int>> hm(n); // [parent, distance]
+        vector<vector<int>> children(n); // Store children for each node
+        hm[0] = {n, 0}; // Root node
+        for (int i = 1; i < n; i++) {
+            hm[i] = {i - 1, i}; // Parent and initial distance
+            children[i - 1].push_back(i); // Add child relationship
+        }
+        vector<int> rt(queries.size());
+        for (int j = 0; j < queries.size(); j++) {
+            int newParent = queries[j][0];
+            int node = queries[j][1];
+            children[newParent].push_back(node);
+            int nps = hm[newParent].second + 1;
+            if (hm[node].second>nps) {
+                hm[node].first = newParent;
+                hm[node].second = hm[newParent].second + 1;
+                queue<int> q;
+                q.push(node); // add the tail of query edge to queue list
+                while (!q.empty()) {
+                    int curr = q.front(); // get the first element of queue
+                    q.pop(); //remove it
+                    for (int child : children[curr]) { //children[curr] gets all the chrildren node of curr
+                    int updated = hm[curr].second + 1;
+                    if(hm[child].second>updated){
+                        hm[child].second = updated;
+                        q.push(child);
+                        }
+                    }
+                }
+            }
+            rt[j] = hm[n - 1].second;
+        }
+        return rt;
+    }
+};
