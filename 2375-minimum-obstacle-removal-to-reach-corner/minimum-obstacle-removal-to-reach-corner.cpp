7ad4e10c16e3ead48a94fda8480490731b86a61f
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-11-28 22:31:06 +1100

    Time: 239 ms (76.85%) | Memory: 108.7 MB (70.38%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -5,21 +5,17 @@ public:
         int n = grid[0].size();
         vector<int> directions = {0, 1, 0, -1, 0}; // Directions: right, down, left, up
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq; // Min-heap
-        vector<vector<int>> dist(m, vector<int>(n, INT_MAX)); // Distance matrix
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX)); 
         dist[0][0] = 0;
-        pq.push({0, {0, 0}}); // (current weight, (x, y))
-
+        pq.push({0, {0, 0}}); 
         while (!pq.empty()) {
             auto [currentWeight, coords] = pq.top();
             auto [x, y] = coords;
             pq.pop();
-
             // If we've already found a better way to this cell, skip processing
             if (currentWeight > dist[x][y]) continue;
-
             // Check if we reached the destination
             if (x == m - 1 && y == n - 1) return currentWeight;
-
             for (int i = 0; i < 4; ++i) {
                 int nx = x + directions[i], ny = y + directions[i + 1];
                 if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
