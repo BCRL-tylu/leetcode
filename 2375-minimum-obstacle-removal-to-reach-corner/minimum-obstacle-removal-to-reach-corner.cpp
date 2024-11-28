4db925c01ebd1313e6ee6e13a29d4e135e956eff
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-11-28 22:16:56 +1100

    Time: 268 ms (70.37%) | Memory: 109.9 MB (69.12%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -12,22 +12,20 @@ public:
         while (!pq.empty()) {
             auto [currentWeight, x, y] = pq.top();
             pq.pop();
-            if (x == m - 1 && y == n - 1) return currentWeight;
             if (currentWeight > dist[x][y]) continue;
             for (int i = 0; i < 4; ++i) {
                 int nx = x + directions[i], ny = y + directions[i + 1];
-
                 if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
                     int newWeight = currentWeight + grid[nx][ny]; // Add edge weight
                     // If this path is better, update distance and push to the heap
                     if (newWeight < dist[nx][ny]) {
                         dist[nx][ny] = newWeight;
+                        if (nx == m - 1 && ny == n - 1) return newWeight;
                         pq.emplace(newWeight, nx, ny);
                     }
                 }
             }
         }
-
         return -1; // If no path to the bottom-right corner exists
     }
 };
