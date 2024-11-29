d6fb41a0a05b51a42da096525d8eb4f8d60ceab6
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-11-29 15:23:15 +1100

    Time: 1188 ms (5.15%) | Memory: 43.3 MB (100.00%) - LeetSync

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -6,15 +6,20 @@ public:
         int dist[m][n];
         memset(dist,10001,sizeof(dist));
         dist[0][0] = 0;
+
+        int current[m][n];
+        memset(current,-1,sizeof(dist));
+        current[0][0] = 0;
+
         // Priority queue: {distance, {row, col}}
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<>> pq;
-        pq.push({0, {0, 0}});
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+        pq.push({0, 0});
         // Directions: {right, down, left, up}
         int d[5] = {0, 1, 0, -1, 0};
         while (!pq.empty()) {
-            auto [currentDist, cell] = pq.top();
+            auto [x,y] = pq.top();
+            int currentDist = current[x][y];
             pq.pop();
-            int x = cell.first, y = cell.second;
             // Early exit if we reached the destination
             if (x == m - 1 && y == n - 1) return currentDist;
             // Skip if already processed
@@ -29,7 +34,8 @@ public:
                     int nextDist = newdist + waitTime;
                     if (nextDist < dist[nx][ny]) {
                         dist[nx][ny] = nextDist;
-                        pq.push({nextDist, {nx, ny}});
+                        current[nx][ny] = nextDist;
+                        pq.push({nx, ny});
                     }
                 }
             }
