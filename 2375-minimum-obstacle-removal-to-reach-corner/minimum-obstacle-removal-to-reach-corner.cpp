7d954ed5c0ab039ef473fc3911d719e11a0a147f
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-02 21:02:54 +1100

    Time: 36 ms (99.64%) | Memory: 91.9 MB (99.88%) - LeetSync

--- 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -1,35 +1,80 @@
-class Solution {
-public:
-    int minimumObstacles(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int directions[5] = {0, 1, 0, -1, 0}; // Directions right 1,0, down0,-1, left-1,0, up 0,1
-        int dist[m][n];
-        memset(dist,-1,sizeof(dist));
-        dist[0][0] = 0;
-        deque<pair<int, int>> dq; // Deque for 0-1 BFS
-        dq.emplace_front(0, 0); // Starting point
-        while (!dq.empty()) {
-            auto [x, y] = dq.front();
-            dq.pop_front();
-            // Check if we reached the destination
-            if (x == m - 1 && y == n - 1) return dist[x][y];
-            // Explore neighbors
-            for (int i = 0; i < 4; ++i) {
-                int nx = x + directions[i], ny = y + directions[i + 1];
-                // if the new position is within boundry and new position hasn't been updated
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n && dist[nx][ny]==-1) {
-                    // If this path is better, update distance
-                        dist[nx][ny] = dist[x][y] + grid[nx][ny];
-                        // Add to the front or back of the deque based on obstacle weight
-                        if (grid[nx][ny]) {
-                            dq.emplace_back(nx, ny); //Obstacle, lower priority
-                        } else {
-                            dq.emplace_front(nx, ny); //No obstacle, higher priority
-                        }
-                }
-            }
-        }
-        return -1; // If no path to the bottom-right corner exists
+#define NO_SAN __attribute__((no_sanitize("undefined", "address", "coverage", "thread")))
+#define INL __attribute__((always_inline))
+#define HOT __attribute__((hot))
+#define INL_ATTR noexcept INL NO_SAN HOT
+#define OUTL_ATTR noexcept NO_SAN HOT
+
+template <class T, class S, S cap>
+struct ring_queue {
+    S start, end;
+    T elem[cap];
+
+    ring_queue() INL_ATTR: start(0), end(0) {}
+
+    void push(const T v) INL_ATTR {
+        elem[end++] = v;
+        if (end >= cap) end = 0;
+    }
+
+    T pop() INL_ATTR {
+        const T r = elem[start++];
+        if (start >= cap) start = 0;
+        return r;
+    }
+
+    bool empty() INL_ATTR {
+        return start == end;
     }
 };
+
+class Solution {
+public:
+    static int minimumObstacles(vector<vector<int>>& grid) OUTL_ATTR {
+        const uint m = grid.size(), n = grid.front().size();
+        const uint8_t nord = 32 - countl_zero(n);
+        const uint cmask = (1u << nord) - 1u, endcoo = ((m - 1u) << nord) + n - 1u;
+        constexpr uint QCAP = 100'000;
+        ring_queue<uint, uint, QCAP> que[2];
+        que[0].push(0);
+        uint cost = 0;
+        bool a = false;
+        grid.front().front() = -1;
+        
+        while (!que[a].empty()) {
+            while (!que[a].empty()) {
+                const uint coo = que[a].pop();
+                const uint i = coo >> nord, j = coo & cmask;
+                if (int b = !!i, &g = grid[i-b][j]; b & g >= 0) {
+                    que[a^g].push(coo - cmask - 1u);
+                    g = -1;
+                }
+                if (int b = i + 1u < m, &g = grid[i+b][j]; b & g >= 0) {
+                    if (coo + cmask + 1u == endcoo) [[unlikely]]
+                        return cost + g;
+                    que[a^g].push(coo + cmask + 1u);
+                    g = -1;
+                }
+                if (int b = !!j, &g = grid[i][j-b]; b & g >= 0) {
+                    que[a^g].push(coo - 1u);
+                    g = -1;
+                }
+                if (int b = j + 1u < n, &g = grid[i][j+b]; b & g >= 0) {
+                    if (coo + 1u == endcoo) [[unlikely]]
+                        return cost + g;
+                    que[a^g].push(coo + 1u);
+                    g = -1;
+                }
+            }
+            cost++;
+            a = !a;
+        }
+        return -1;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
