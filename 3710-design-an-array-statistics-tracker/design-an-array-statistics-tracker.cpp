366a4fa5522bf643b78e048a5cd8e1c875667dd6
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-03 12:26:55 +1100

    Time: 544 ms (56.25%) | Memory: 364.8 MB (37.50%) - LeetSync

--- 3710-design-an-array-statistics-tracker/design-an-array-statistics-tracker.cpp
+++ 3710-design-an-array-statistics-tracker/design-an-array-statistics-tracker.cpp
@@ -1,111 +1,94 @@
-#include <deque>
-#include <unordered_map>
-#include <set>
-#include <iostream>
-#include <queue>
-#include <algorithm>
+class cmp {
+public:
+    bool operator() (const pair<int, int>& a, const pair<int, int>& b) const {
+        if (a.first != b.first) {
+            return a.first < b.first;
+        } else {
+            return a.second > b.second;
+        }
+    }
+};
 
 class StatisticsTracker {
 private:
-    std::deque<int> numbers;                                   // To store numbers in insertion order.
-    std::unordered_map<int, int> frequencyMap;                // To track frequency of numbers.
-    std::unordered_map<int, std::set<int>> freqMap;           // Maps frequencies to the corresponding numbers.
-    long long sum;                                            // Sum of all numbers.
-    int maxFrequency;                                         // Track maximum frequency.
-    std::multiset<int> left;                                 // Multiset for the lower half (max heap).
-    std::multiset<int> right;                                // Multiset for the upper half (min heap).
+    long long sum;
+    deque<int> nums;
+    multiset<int, greater<>> upper;             // min heap (for bigger nums)
+    multiset<int> lower;                        // max heap (for smaller nums)
+    unordered_map<int, int> numFreq;            // {num, freq}
+    set<pair<int, int>, cmp> freqNum;           // {freq, num}
 
     void balanceHeaps() {
-        // Ensure the left heap has at most one more element than the right.
-        if (left.size() > right.size() + 1) {
-            right.insert(*left.rbegin());  // Insert the largest from left to right.
-            left.erase(--left.end());       // Remove the largest from left.
-        } else if (right.size() > left.size()) {
-            left.insert(*right.begin());    // Insert the smallest from right to left.
-            right.erase(right.begin());      // Remove the smallest from right.
+        if (upper.size() == lower.size() - 1) { // if upper over erase
+            upper.insert(*prev(lower.end()));
+            lower.erase(prev(lower.end()));
+        }
+        if (lower.size() == upper.size() - 2) { // if lower over erase
+            lower.insert(*prev(upper.end()));
+            upper.erase(prev(upper.end()));
         }
     }
 
 public:
-    StatisticsTracker() : sum(0), maxFrequency(0) {}
-
+    StatisticsTracker() : sum(0) {}
+    
     void addNumber(int number) {
-        numbers.push_back(number);
-        frequencyMap[number]++;
-        int freq = frequencyMap[number];
-        
-        // Update the sum
+        // maintain sum and deque
         sum += number;
+        nums.push_back(number);
 
-        // Update frequency map
-        if (freq > 1) {
-            // Remove the number from the previous frequency set
-            freqMap[freq - 1].erase(number);
-            if (freqMap[freq - 1].empty()) {
-                freqMap.erase(freq - 1); // Clean up empty sets.
-            }
+        // maintain freq
+        int prevFreq = numFreq[number]++;
+        int currFreq = prevFreq + 1;
+        if (prevFreq != 0) {
+            freqNum.erase({prevFreq, number});
         }
-        // Add the number to the current frequency set
-        freqMap[freq].insert(number);
-        // Update the maximum frequency
-        maxFrequency = std::max(maxFrequency, freq);
+        freqNum.insert({currFreq, number});
 
-        // Insert into the appropriate multiset
-        if (left.empty() || number <= *left.rbegin()) {
-            left.insert(number);
-        } else {
-            right.insert(number);
+        // maintain two heap
+        upper.insert(number);
+        lower.insert(*prev(upper.end()));
+        upper.erase(prev(upper.end()));
+        if (upper.size() < lower.size()) {
+            upper.insert(*prev(lower.end()));
+            lower.erase(prev(lower.end()));
         }
-        // Balance the heaps
-        balanceHeaps();
     }
-
+    
     void removeFirstAddedNumber() {
-        int numberToRemove = numbers.front();
-        numbers.pop_front(); // Remove the first element
-
-        // Decrease the count in frequency map
-        int freq = frequencyMap[numberToRemove];
-        sum -= numberToRemove;
-        frequencyMap[numberToRemove]--;
-
-        // Update frequency map
-        freqMap[freq].erase(numberToRemove);
-        if (freqMap[freq].empty()) {
-            freqMap.erase(freq); // Clean up empty sets.
-        }
-        if (freq > 1) {
-            freqMap[freq - 1].insert(numberToRemove);
-        }
-
-        // Update the maximum frequency if necessary
-        if (freq == maxFrequency && freqMap.find(maxFrequency) == freqMap.end()) {
-            maxFrequency--; // Decrease max frequency if it no longer exists
-        }
-
-        // Determine from which multiset to remove the number
-        if (left.count(numberToRemove)) {
-            left.erase(left.find(numberToRemove));
+        // maintain sum and deque
+        int number = nums.front(); // the number to remove
+        nums.pop_front();
+        sum -= number;
+        
+        // maintain freq
+        int prevFreq = numFreq[number]--;
+        int currFreq = prevFreq - 1;
+        freqNum.erase({prevFreq, number});
+        if (currFreq != 0) {
+            freqNum.insert({currFreq, number});
         } else {
-            right.erase(right.find(numberToRemove));
+            numFreq.erase(number);
         }
-        // Rebalance the heaps after removal
+
+        // maintain two heap
+        if (number >= *prev(upper.end())) {
+            upper.erase(upper.find(number));
+        } else {
+            lower.erase(lower.find(number));
+        }
         balanceHeaps();
     }
-
+    
     int getMean() {
-        return sum / numbers.size(); // Integer division gives the floored mean
+        return sum / nums.size();
     }
-
+    
     int getMedian() {
-        if (left.size() == right.size()) {
-            return *right.begin(); // Return the larger median (top of right)
-        } else {
-            return *left.rbegin(); // Return the top of left
-        }
+        return *prev(upper.end());
     }
-
+    
     int getMode() {
-        return *freqMap[maxFrequency].begin(); // Smallest mode
+        return prev(freqNum.end())->second;
     }
 };
