c4c3e2a3128080741897cae6860d3149fde3e62f
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-04 12:23:55 +1100

    Time: 0 ms (100.00%) | Memory: 17.1 MB (7.53%) - LeetSync

--- 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,32 +1,27 @@
 #include <iostream>
-#include <string>
-using namespace std;
 
 class Solution {
 public:
-    char nextCharCyclic(char ch) {
-        return (ch - 'a' + 1) % 26 + 'a'; // Direct cyclic calculation, assumes valid 'a'-'z'.
-    }
+
 
     bool canMakeSubsequence(string str1, string str2) {
         int n = str1.size();
         int m = str2.size();
         int cp = 0;
-
-        for (int i = 0; i < m; ++i) {
-            bool found = false;
-            while (cp < n) {
-                if (str2[i] == str1[cp] || nextCharCyclic(str1[cp]) == str2[i]) {
-                    ++cp; // Move cp forward to avoid re-checking.
+        bool found;
+        for(int i =0;i<m;i++){
+            found = false;
+            for(int j = cp; j<n;j++){
+                if(str2[i] == str1[j] || ((str1[j] - 'a' + 1) % 26 + 'a')==str2[i]){
+                    cp = j+1;
                     found = true;
                     break;
                 }
-                ++cp; // Skip the unmatched character.
             }
-            if (!found) {
+            if(!found){
                 return false;
             }
         }
-        return true;
+        return found;
     }
 };
