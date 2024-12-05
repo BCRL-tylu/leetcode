65ec7838fea5dee290ebf6aa1da2c2b8515b44e2
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-05 17:51:40 +1100

    Time: 16 ms (41.38%) | Memory: 21.9 MB (33.33%) - LeetSync

--- 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
+++ 2414-move-pieces-to-obtain-a-string/move-pieces-to-obtain-a-string.cpp
@@ -1,65 +1,23 @@
-#include <string>
-#include <iostream>
-
 class Solution {
 public:
-    bool canChange(std::string start, std::string target) {
-        int n = start.size();
-        int sp = 0; // Pointer for start
-        int tp = 0; // Pointer for target
+    bool canChange(string start, string target) {
+        int index , r = 0, l = 0;
+        int size = target.length();
+        for (int i = 0, j = 0; i < size; i++){
+            if (start[i]=='R') r++;
+            if (start[i]=='L') l++;
+            if (target[i]=='R') r--;
+            if (target[i]=='L') l--;
 
-        while (sp < n && tp < n) {
-            // Skip underscores in start
-            while (sp < n && start[sp] == '_') {
-                sp++;
+            if (start[i]!='_'){
+                index = i;
+                while (j < size && target[j]=='_') j++;
+                if(start[i]!=target[j] || (start[i]=='R' && index > j) || (start[i]=='L' && index < j)) return false;
+                j++;
             }
-            // Skip underscores in target
-            while (tp < n && target[tp] == '_') {
-                tp++;
-            }
-
-            // If both pointers are at the end, it means we've matched all
-            if (sp == n && tp == n) {
-                return true;
-            }
-
-            // If we reached the end of one but not the other, return false
-            if (sp == n || tp == n) {
-                return false;
-            }
-
-            // Check if characters match
-            if (start[sp] != target[tp]) {
-                return false; // Mismatched characters
-            }
-
-            // Check the direction of movement for 'L' and 'R'
-            if (start[sp] == 'L') {
-                // 'L' should not move to the right
-                if (sp < tp) {
-                    return false; // Invalid move
-                }
-            } else if (start[sp] == 'R') {
-                // 'R' should not move to the left
-                if (sp > tp) {
-                    return false; // Invalid move
-                }
-            }
-
-            // Move to the next character
-            sp++;
-            tp++;
         }
-        
-        // Check if we have valid characters left
-        while (sp < n && start[sp] == '_') {
-            sp++;
-        }
-        while (tp < n && target[tp] == '_') {
-            tp++;
-        }
-        
-        return sp == n && tp == n; // Both pointers should be at the end
+
+        if (r == 0 && l == 0) return true;
+        else return false;
     }
 };
-
