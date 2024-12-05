5daa04c8e3db879fdc8ca98c602e7e36a351556a
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-06 02:02:06 +1100

    Time: 63 ms (91.94%) | Memory: 137.3 MB (82.26%) - LeetSync

--- 3652-find-sorted-submatrices-with-maximum-element-at-most-k/find-sorted-submatrices-with-maximum-element-at-most-k.cpp
+++ 3652-find-sorted-submatrices-with-maximum-element-at-most-k/find-sorted-submatrices-with-maximum-element-at-most-k.cpp
@@ -1,54 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
 class Solution {
 public:
-    long long countSubmatrices(vector<vector<int>>& grid, int k) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-
-        int bar[rows][cols];
-        for(int row = 0; row < rows; row++) {
-            for(int col = cols - 1; col >= 0; col--) {
-                if(grid[row][col] > k) {
-                    bar[row][col] = 0;
-                } else {
-                    bar[row][col] = 1;
-                    if(col < cols - 1 && grid[row][col + 1] <= grid[row][col]) {
-                        bar[row][col] += bar[row][col + 1];
-                    }
+long long countSubmatrices(vector<vector<int>>& g, int k) {
+    long long m = g.size(), n = g[0].size(), res = 0;
+    vector<vector<pair<int, int>>> mono(n);
+    vector<int> mono_sum(n);
+    for (int i = 0; i < m; ++i)
+        for (int j = 0, len = 0; j < n; ++j)
+            if (g[i][j] <= k) {
+                len = (j && g[i][j - 1] >= g[i][j] ? len : 0) + 1;
+                int len_count = 1;
+                while (!mono[j].empty() && mono[j].back().first > len) {
+                    len_count += mono[j].back().second;
+                    mono_sum[j] -= mono[j].back().first * mono[j].back().second; 
+                    mono[j].pop_back();
                 }
+                if (mono[j].empty() || mono[j].back().first != len)
+                    mono[j].push_back({len, 0});
+                mono[j].back().second += len_count;
+                mono_sum[j] += len * len_count;
+                res += mono_sum[j];
             }
-        }
-
-        long long totalSum = 0;
-        for(int col = 0; col < cols; col++) {
-            
-            vector<pair<int, int>> barStack = {{0, -1}}; // {bar, row}
-            int sum = 0;
-
-            for(int row = 0; row < rows; row++) {
-
-                while(bar[row][col] < barStack.back().first) {
-
-                    auto [barSize, barRow] = barStack.back();
-                    barStack.pop_back();
-
-                    int barDiff = barSize - max(barStack.back().first, bar[row][col]);
-                    int boxHeight = row - barRow;
-                    sum -= barDiff * boxHeight;
-
-                    if(barStack.back().first < bar[row][col]) {
-                        barStack.push_back({bar[row][col], barRow});
-                    }
-                }
-
-                sum += bar[row][col];
-                if(bar[row][col] > barStack.back().first) {
-                    barStack.push_back({bar[row][col], row});
-                }
-
-                totalSum += sum;
+            else {
+                mono[j].clear();
+                len = mono_sum[j] = 0;
             }
-        }
-
-        return totalSum;
-    }
+    return res;
+}
 };
