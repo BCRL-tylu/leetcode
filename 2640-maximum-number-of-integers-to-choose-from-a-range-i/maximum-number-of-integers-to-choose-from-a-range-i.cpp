080f0c0915487d35fcb3c4b3481397eaa2b9b044
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-06 11:57:11 +1100

    Time: 0 ms (100.00%) | Memory: 118.9 MB (78.03%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -2,17 +2,23 @@ class Solution {
 public:
     int maxCount(std::vector<int>& banned, int n, int maxSum) {
         std::vector<bool> isBanned(n + 1, false);
+
         // Mark banned numbers using a bit array
         for (int num : banned) {
             if (num <= n) {
                 isBanned[num] = true;
             }
         }
+
         int sum = 0, count = 0;
+
         // Iterate through numbers from 1 to n
         for (int i = 1; i <= n; ++i) {
             if (isBanned[i]) {
                 continue; // Skip banned numbers
+            }
+            if(i>maxSum){
+                break;
             }
             if (sum + i > maxSum) {
                 break; // Stop if adding `i` exceeds `maxSum`
@@ -20,6 +26,7 @@ public:
             sum += i;
             ++count;
         }
+
         return count;
     }
 };
