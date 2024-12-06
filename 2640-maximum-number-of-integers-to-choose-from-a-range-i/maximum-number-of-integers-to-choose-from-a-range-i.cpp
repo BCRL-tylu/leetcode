b016b6e62d2b2cbc3c99f7e43388480c9fb912cf
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-06 12:01:52 +1100

    Time: 7 ms (96.95%) | Memory: 118.7 MB (78.61%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     int maxCount(std::vector<int>& banned, int n, int maxSum) {
         std::vector<bool> isBanned(n + 1, false);
-
-        // Mark banned numbers using a bit array
         for (int num : banned) {
-            if (num <= n) {
+            if (num <= n)
                 isBanned[num] = true;
-            }
         }
 
         int sum = 0, count = 0;
 
-        // Iterate through numbers from 1 to n
         for (int i = 1; i <= n; ++i) {
-            if (isBanned[i]) {
-                continue; // Skip banned numbers
-            }
-            if (i>maxSum||sum + i > maxSum) {
-                break; // Stop if adding `i` exceeds `maxSum`
-            }
+            if (isBanned[i]) 
+                continue;
+            if (sum + i > maxSum) 
+                break;
             sum += i;
             ++count;
         }
