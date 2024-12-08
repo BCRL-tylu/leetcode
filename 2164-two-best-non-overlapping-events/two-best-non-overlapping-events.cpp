2388153a98f86de167c6f8a3f20304d599547c02
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-08 17:35:34 +1100

    Time: 43 ms (96.40%) | Memory: 125.5 MB (84.26%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -8,12 +8,10 @@ class Solution {
 public:
     vector<int> generateMaxValues(const vector<vector<int>>& events) {
         int n = events.size();
-        vector<int> maxValues(n, 0); // Initialize the result vector with size n
-        // Start with the last event's value
+        vector<int> maxValues(n);
         maxValues[n - 1] = events[n - 1][2];
-        // Traverse from the second last element to the start
+
         for (int i = n - 2; i >= 0; --i) {
-            // For each position, take the max of the current value and the next max
             maxValues[i] = max(events[i + 1][2], maxValues[i + 1]);
         }
         return maxValues;
@@ -22,12 +20,11 @@ public:
     int maxTwoEvents(vector<vector<int>>& events) {
         int n = events.size();
         int maxdual = 0;
-        pair<int, int> ind_range;
         int maxind = events[n - 1][2];
 
         // Step 1: Sort events based on the start times
         sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0]; // Compare based on the first element of each sub-array
+            return a[0] < b[0];
         });
 
         // Step 2: Extract sorted first indices into a separate vector
@@ -41,10 +38,7 @@ public:
         for (int i = 0; i < n - 1; i++) {
             // Update the maximum single event value
             if (events[i][2] > maxind) {
-                ind_range = {events[i][0], events[i][1]};
                 maxind = events[i][2];
-            } else if (events[i][0] <= ind_range.first && events[i][1] >= ind_range.second) {
-                continue;
             }
 
             // Use binary search to find the next valid event
@@ -53,9 +47,7 @@ public:
             if (it != sortedFirstIndices.end()) {
                 int startIndex = it - sortedFirstIndices.begin(); // Get the index of the found event
                 int currentdual = maxValues[startIndex - 1];
-                if (currentdual + events[i][2] > maxdual) {
-                    maxdual = events[i][2] + currentdual;
-                }
+                maxdual = max(maxdual, events[i][2] + currentdual);
             }
         }
         return max(maxdual, maxind);
