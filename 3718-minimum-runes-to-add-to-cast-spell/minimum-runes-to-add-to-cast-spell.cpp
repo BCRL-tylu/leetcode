6ce3eb904d7518093950a37f687df5b0b5a899db
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-09 16:43:54 +1100

    Time: 895 ms (100.00%) | Memory: 501.1 MB (100.00%) - LeetSync

--- 3718-minimum-runes-to-add-to-cast-spell/minimum-runes-to-add-to-cast-spell.cpp
+++ 3718-minimum-runes-to-add-to-cast-spell/minimum-runes-to-add-to-cast-spell.cpp
@@ -1,15 +1,22 @@
+#include <unordered_map>
+#include <unordered_set>
+#include <stack>
+#include <vector>
+
 class Solution {
 public:
-    int minRunesToAdd(int n, vector<int>& crystals, vector<int>& flowFrom, vector<int>& flowTo) {
-        vector<vector<int>> adj(n), revAdj(n);
+    int minRunesToAdd(int n, std::vector<int>& crystals, std::vector<int>& flowFrom, std::vector<int>& flowTo) {
+        std::unordered_map<int, std::unordered_set<int>> adj, revAdj;
+
+        // Build adjacency list and reverse adjacency list
         for (int i = 0; i < flowFrom.size(); i++) {
-            adj[flowFrom[i]].push_back(flowTo[i]);
-            revAdj[flowTo[i]].push_back(flowFrom[i]);
+            adj[flowFrom[i]].insert(flowTo[i]);
+            revAdj[flowTo[i]].insert(flowFrom[i]);
         }
 
         // Perform DFS to determine the order of nodes
-        vector<bool> visited(n, false);
-        stack<int> order;
+        std::vector<bool> visited(n, false);
+        std::stack<int> order;
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 dfs(i, adj, visited, order);
@@ -17,9 +24,9 @@ public:
         }
 
         // Find SCCs using the reverse graph
-        vector<int> sccId(n, -1);
+        std::vector<int> sccId(n, -1);
         int sccCount = 0;
-        vector<int> sccProperties; 
+        std::vector<int> sccProperties;
 
         while (!order.empty()) {
             int node = order.top();
@@ -33,15 +40,15 @@ public:
 
         // Mark the SCCs containing crystals
         for (int crystal : crystals) {
-            sccProperties[sccId[crystal]] |= 1;
+            sccProperties[sccId[crystal]] |= 1; // Set crystal bit (bit 0)
         }
 
         // Check incoming edges to SCCs
-        vector<bool> hasIncoming(sccCount, false);
+        std::vector<bool> hasIncoming(sccCount, false);
         for (int i = 0; i < flowFrom.size(); i++) {
             int u = sccId[flowFrom[i]], v = sccId[flowTo[i]];
             if (u != v) {
-                hasIncoming[v] = true; 
+                hasIncoming[v] = true; // Mark incoming edges for SCC v
             }
         }
 
@@ -49,28 +56,34 @@ public:
         int runesNeeded = 0;
         for (int i = 0; i < sccCount; i++) {
             if (!(sccProperties[i] & 1) && !hasIncoming[i]) {
-                runesNeeded++; 
+                runesNeeded++; // Need a rune for this SCC
             }
         }
         return runesNeeded;
     }
 
 private:
-    void dfs(int node, const vector<vector<int>>& adj, vector<bool>& visited, stack<int>& order) {
+    void dfs(int node, const std::unordered_map<int, std::unordered_set<int>>& adj, std::vector<bool>& visited, std::stack<int>& order) {
         visited[node] = true;
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                dfs(neighbor, adj, visited, order);
+        auto it = adj.find(node);
+        if (it != adj.end()) {
+            for (int neighbor : it->second) { // Access the set safely
+                if (!visited[neighbor]) {
+                    dfs(neighbor, adj, visited, order);
+                }
             }
         }
-        order.push(node); 
+        order.push(node); // Push node to the stack after finishing its neighbors
     }
 
-    void shrinkNodes(int node, const vector<vector<int>>& revAdj, vector<int>& sccId, vector<int>& sccProperties, int sccCount) {
-        sccId[node] = sccCount; 
-        for (int neighbor : revAdj[node]) {
-            if (sccId[neighbor] == -1) {
-                shrinkNodes(neighbor, revAdj, sccId, sccProperties, sccCount); 
+    void shrinkNodes(int node, const std::unordered_map<int, std::unordered_set<int>>& revAdj, std::vector<int>& sccId, std::vector<int>& sccProperties, int sccCount) {
+        sccId[node] = sccCount; // Mark the current node with its SCC ID
+        auto it = revAdj.find(node);
+        if (it != revAdj.end()) {
+            for (int neighbor : it->second) { // Access the set safely
+                if (sccId[neighbor] == -1) {
+                    shrinkNodes(neighbor, revAdj, sccId, sccProperties, sccCount); // Recursively mark all nodes in this SCC
+                }
             }
         }
     }
