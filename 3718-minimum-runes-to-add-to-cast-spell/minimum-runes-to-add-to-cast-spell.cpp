40b1167a51c0ca86f843e58062f6811c2a06d1e7
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-09 16:38:10 +1100

    Time: 261 ms (100.00%) | Memory: 285.6 MB (100.00%) - LeetSync

--- 3718-minimum-runes-to-add-to-cast-spell/minimum-runes-to-add-to-cast-spell.cpp
+++ 3718-minimum-runes-to-add-to-cast-spell/minimum-runes-to-add-to-cast-spell.cpp
@@ -1,54 +1,72 @@
 class Solution {
-    void dfs(const vector<vector<int>> &con, int x, vector<bool> &mark, vector<int> &all) {
-        if (mark[x]) return;
-        mark[x] = true;
-        for (int y : con[x]) {
-            dfs(con, y, mark, all);
-        } 
-        all.push_back(x);
-    }
-    
-    void dfs(const vector<vector<int>> &con, int x, vector<int> &id, int m) {
-        if (id[x] >= 0) return;
-        id[x] = m;
-        for (int y : con[x]) {
-            dfs(con, y, id, m);
-        }
-    }
 public:
     int minRunesToAdd(int n, vector<int>& crystals, vector<int>& flowFrom, vector<int>& flowTo) {
-        vector<vector<int>> con(n), rcon(n);
-        for (int i = 0; i < flowFrom.size(); ++i) {
-            con[flowFrom[i]].push_back(flowTo[i]);
-            rcon[flowTo[i]].push_back(flowFrom[i]);
+        // Build adjacency list and reverse adjacency list
+        vector<vector<int>> adj(n), revAdj(n);
+        for (int i = 0; i < flowFrom.size(); i++) {
+            adj[flowFrom[i]].push_back(flowTo[i]);
+            revAdj[flowTo[i]].push_back(flowFrom[i]);
         }
-        vector<bool> mark(n);
-        vector<int> all;
-        for (int i = 0; i < n; ++i) {
-            dfs(con, i, mark, all);
-        }
-        vector<int> id(n, -1);
-        int m = 0;
-        for (int i = n - 1; i >= 0; --i) {
-            const int x = all[i];
-            if (id[x] < 0) {
-                dfs(rcon, x, id, m++);
+        // Perform DFS to determine the order of nodes
+        vector<bool> visited(n, false);
+        stack<int> order;
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited, order);
             }
         }
-        vector<bool> to(m);
-        for (int i = 0; i < flowFrom.size(); ++i) {
-            if (id[flowFrom[i]] != id[flowTo[i]]) {
-                to[id[flowTo[i]]] = true;
+        //Find SCCs using the reverse graph
+        vector<int> sccId(n, -1);
+        int sccCount = 0;
+        vector<int> sccProperties; // To store properties of each SCC
+        while (!order.empty()) {
+            int node = order.top();
+            order.pop();
+            if (sccId[node] == -1) {
+                sccProperties.push_back(0); // Initialize new SCC properties
+                shrinkNodes(node, revAdj, sccId, sccCount, sccProperties);
+                sccCount++;
             }
         }
-        vector<bool> special(m);
-        for (int x : crystals) {
-            special[id[x]] = true;
+        //Mark the SCCs containing crystals
+        for (int crystal : crystals) {
+            sccProperties[sccId[crystal]] |= 1; // Set crystal bit (bit 0)
         }
-        int r = 0;
-        for (int i = 0; i < m; ++i) {
-            r += (!to[i] && !special[i]);
+        //Check incoming edges to SCCs
+        vector<bool> hasIncoming(sccCount, false);
+        for (int i = 0; i < flowFrom.size(); i++) {
+            int u = sccId[flowFrom[i]], v = sccId[flowTo[i]];
+            if (u != v) {
+                hasIncoming[v] = true; // Mark incoming edges for SCC v
+            }
         }
-        return r;
+        // Count SCCs that do not have crystals and do not have incoming edges
+        int runesNeeded = 0;
+        for (int i = 0; i < sccCount; i++) {
+            // Check if the SCC has no crystals and no incoming edges
+            if (!(sccProperties[i] & 1) && !hasIncoming[i]) {
+                runesNeeded++; // Need a rune for this SCC
+            }
+        }
+        return runesNeeded;
+    }
+private:
+    void dfs(int node, const vector<vector<int>>& adj, vector<bool>& visited, stack<int>& order) {
+        visited[node] = true;
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                dfs(neighbor, adj, visited, order);
+            }
+        }
+        order.push(node); // Push node to the stack after finishing its neighbors
+    }
+    void shrinkNodes(int node, const vector<vector<int>>& revAdj, vector<int>& sccId, int sccCount, vector<int>& sccProperties) {
+        sccId[node] = sccCount; // Mark the current node with its SCC ID
+        // Collect nodes in the current SCC and check for incoming edges
+        for (int neighbor : revAdj[node]) {
+            if (sccId[neighbor] == -1) {
+                shrinkNodes(neighbor, revAdj, sccId, sccCount, sccProperties); // Recursively mark all nodes in this SCC
+            }
+        }
     }
 };
