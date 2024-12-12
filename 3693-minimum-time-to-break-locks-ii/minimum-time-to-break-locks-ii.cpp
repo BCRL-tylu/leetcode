d98a94220b96aae4d5002dbe54a0fe5830500cc3
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-12 14:38:07 +1100

    Time: 35 ms (100.00%) | Memory: 26.7 MB (100.00%) - LeetSync

--- 3693-minimum-time-to-break-locks-ii/minimum-time-to-break-locks-ii.cpp
+++ 3693-minimum-time-to-break-locks-ii/minimum-time-to-break-locks-ii.cpp
@@ -1,85 +1,173 @@
-#include <vector>
-#include <cmath>
-#include <climits>
-#include <algorithm>
-#include <iostream>
+template <typename T>
+struct hungarian
+{ // km
+    int n;
+    vector<int> matchx;
+    vector<int> matchy;
+    vector<int> pre;
+    vector<bool> visx;
+    vector<bool> visy;
+    vector<T> lx;
+    vector<T> ly;
+    vector<vector<T>> g;
+    vector<T> slack;
+    T inf;
+    T res;
+    queue<int> q;
+    int org_n;
+    int org_m;
+
+    hungarian(int _n, int _m)
+    {
+        org_n = _n;
+        org_m = _m;
+        n = max(_n, _m);
+        inf = numeric_limits<T>::max();
+        res = 0;
+        g = vector<vector<T>>(n, vector<T>(n));
+        matchx = vector<int>(n, -1);
+        matchy = vector<int>(n, -1);
+        pre = vector<int>(n);
+        visx = vector<bool>(n);
+        visy = vector<bool>(n);
+        lx = vector<T>(n, -inf);
+        ly = vector<T>(n);
+        slack = vector<T>(n);
+    }
+
+    void addEdge(int u, int v, int w)
+    {
+        g[u][v] = max(w, 0); 
+    }
+
+    bool check(int v)
+    {
+        visy[v] = true;
+        if (matchy[v] != -1)
+        {
+            q.push(matchy[v]);
+            visx[matchy[v]] = true;
+            return false;
+        }
+        while (v != -1)
+        {
+            matchy[v] = pre[v];
+            swap(v, matchx[pre[v]]);
+        }
+        return true;
+    }
+
+    void bfs(int i)
+    {
+        while (!q.empty())
+        {
+            q.pop();
+        }
+        q.push(i);
+        visx[i] = true;
+        while (true)
+        {
+            while (!q.empty())
+            {
+                int u = q.front();
+                q.pop();
+                for (int v = 0; v < n; v++)
+                {
+                    if (!visy[v])
+                    {
+                        T delta = lx[u] + ly[v] - g[u][v];
+                        if (slack[v] >= delta)
+                        {
+                            pre[v] = u;
+                            if (delta)
+                            {
+                                slack[v] = delta;
+                            }
+                            else if (check(v))
+                            {
+                                return;
+                            }
+                        }
+                    }
+                }
+            }
+            T a = inf;
+            for (int j = 0; j < n; j++)
+            {
+                if (!visy[j])
+                {
+                    a = min(a, slack[j]);
+                }
+            }
+            for (int j = 0; j < n; j++)
+            {
+                if (visx[j])
+                { // S
+                    lx[j] -= a;
+                }
+                if (visy[j])
+                { // T
+                    ly[j] += a;
+                }
+                else
+                { // T'
+                    slack[j] -= a;
+                }
+            }
+            for (int j = 0; j < n; j++)
+            {
+                if (!visy[j] && slack[j] == 0 && check(j))
+                {
+                    return;
+                }
+            }
+        }
+    }
+
+    int solve()
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                lx[i] = max(lx[i], g[i][j]);
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            fill(slack.begin(), slack.end(), inf);
+            fill(visx.begin(), visx.end(), false);
+            fill(visy.begin(), visy.end(), false);
+            bfs(i);
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (g[i][matchx[i]] > 0)
+            {
+                res += g[i][matchx[i]];
+            }
+            else
+            {
+                matchx[i] = -1;
+            }
+        }
+        return res;
+    }
+};
 
 class Solution {
+    
 public:
-    int findMinimumTime(std::vector<int>& strength) {
-        int n = strength.size(); // Number of locks
-        std::vector<std::vector<int>> weight(n, std::vector<int>(n)); // Weight matrix
-
-        // Step 0: Construct the weight matrix
+    int findMinimumTime(vector<int>& strength) {
+        const int n = strength.size();
+        hungarian<int> h(n, n);
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < n; ++j) {
-                weight[i][j] = (int)std::ceil((double)strength[j] / (i + 1));
+                h.addEdge(i, j, 1000000 - (strength[i] + j) / (j + 1));
             }
         }
-
-        // Hungarian algorithm variables
-        std::vector<int> y_sword(n, 0), y_lock(n, 0); // Potentials for swords and locks
-        std::vector<int> matched_lock(n, -1);         // Matched lock for each sword
-        std::vector<int> matched_sword(n, -1);       // Matched sword for each lock
-
-        // Hungarian Algorithm Main Loop
-        for (int sword = 0; sword < n; ++sword) {
-            std::vector<int> min_slack(n, INT_MAX), slack_owner(n, -1);
-            std::vector<bool> visited_sword(n, false), visited_lock(n, false);
-
-            int current_sword = sword, current_lock = -1;
-            while (true) {
-                visited_sword[current_sword] = true;
-                int delta = INT_MAX, next_lock = -1;
-
-                for (int lock = 0; lock < n; ++lock) {
-                    if (!visited_lock[lock]) {
-                        int cost = weight[current_sword][lock] - y_sword[current_sword] - y_lock[lock];
-                        if (cost < min_slack[lock]) {
-                            min_slack[lock] = cost;
-                            slack_owner[lock] = current_sword;
-                        }
-                        if (min_slack[lock] < delta) {
-                            delta = min_slack[lock];
-                            next_lock = lock;
-                        }
-                    }
-                }
-
-                if (delta == INT_MAX) {
-                    return -1;
-                }
-
-                for (int i = 0; i < n; ++i) {
-                    if (visited_sword[i]) y_sword[i] += delta;
-                    if (visited_lock[i]) y_lock[i] -= delta;
-                    else min_slack[i] -= delta;
-                }
-
-                current_lock = next_lock;
-                visited_lock[current_lock] = true;
-                int sword_owner = slack_owner[current_lock];
-
-                if (matched_sword[current_lock] == -1) {
-                    while (current_lock != -1) {
-                        int prev_lock = matched_lock[sword_owner];
-                        matched_sword[current_lock] = sword_owner;
-                        matched_lock[sword_owner] = current_lock;
-                        current_lock = prev_lock;
-                        sword_owner = (current_lock != -1) ? slack_owner[current_lock] : -1;
-                    }
-                    break;
-                }
-
-                current_sword = matched_sword[current_lock];
-            }
-        }
-
-        // Calculate the total minimum time
-        int total_time = 0;
-        for (int i = 0; i < n; ++i) {
-            total_time += weight[i][matched_lock[i]];
-        }
-        return total_time;
+        return n * 1000000LL - h.solve();
     }
 };
