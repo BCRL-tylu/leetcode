905ae0ce9d29e737c1759e4ad6061b35ca2f7305
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-12 16:05:17 +1100

    Time: 0 ms (100.00%) | Memory: 12.9 MB (89.48%) - LeetSync

--- 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/take-gifts-from-the-richest-pile.cpp
@@ -1,32 +1,17 @@
-#include <vector>
-#include <set>
-#include <cmath>
-
 class Solution {
 public:
-    long long pickGifts(std::vector<int>& gifts, int k) {
-        std::multiset<int> giftPiles(gifts.begin(), gifts.end());
-
-        for (int i = 0; i < k; ++i) {
-            // Get the largest pile
-            auto it = giftPiles.end(); 
-            --it; // Move to the last element (largest)
-            int maxGifts = *it;
-
-            // Calculate the remaining gifts after taking
-            int remainingGifts = std::floor(std::sqrt(maxGifts));
-
-            // Remove the largest pile and insert the remaining gifts
-            giftPiles.erase(it);
-            giftPiles.insert(remainingGifts);
+    long long pickGifts(vector<int>& gifts, int k) {
+        priority_queue<int>q;
+        long long ans=0;
+        for(int i:gifts){q.push(i);ans+=i;}
+        while(k--)
+        {
+            int v=q.top();
+            ans-=(v-sqrt(v));
+            q.pop();
+            q.push(sqrt(v));
         }
-
-        // Calculate the total gifts remaining
-        long long totalRemaining = 0;
-        for (int giftsCount : giftPiles) {
-            totalRemaining += giftsCount;
-        }
-
-        return totalRemaining;
+        return ans;
+        
     }
 };
