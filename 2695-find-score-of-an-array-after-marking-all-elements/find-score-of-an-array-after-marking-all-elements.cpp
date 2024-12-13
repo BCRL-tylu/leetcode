6a78eebf02e14c9407c5e704e6c156163b4c0952
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-13 12:52:46 +1100

    Time: 0 ms (100.00%) | Memory: 92.1 MB (99.67%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,31 +1,14 @@
 std::vector<bool> marked(1e5 + 1, true);
 class Solution {
 public:
-    long long findScore(vector<int>& nums) {
-        std::priority_queue<std::pair<int, int>,
-                            std::vector<std::pair<int, int>>,
-                            std::greater<std::pair<int, int>>>
-            pq;
-        int n = nums.size();
-        // Fill the min-heap with values and their indices
-        for (int i = 0; i < n; ++i) {
-            pq.emplace(nums[i], i);
-        }
+    long long findScore(vector<int> &nums) {
         long long ans = 0;
-        for (int i = 0; i < n; i++) {
-            auto pair = pq.top();
-            pq.pop();
-            int temp = pair.second;
-            if (marked[temp]) {
-                ans += pair.first;
-                if (temp > 0) {
-                    marked[temp - 1] = false;
-                }
-                marked[temp + 1] = false;
-            }
-        }
-        for(int i = 0;i<n+1;i++){
-            marked[i] = true;
+        for (int i = 0, n = nums.size(); i < n; i += 2) { // i 选了 i+1 不能选
+            int i0 = i;
+            while (i + 1 < n && nums[i] > nums[i + 1]) // 找到下坡的坡底
+                ++i;
+            for (int j = i; j >= i0; j -= 2) // 从坡底 i 到坡顶 i0，每隔一个累加
+                ans += nums[j];
         }
         return ans;
     }
