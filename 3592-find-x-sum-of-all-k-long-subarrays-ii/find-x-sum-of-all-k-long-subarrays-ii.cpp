71485320b5ece3730031d639e7645b6bcc4a4ecb
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-15 01:22:01 +1100

    Time: 590 ms (91.68%) | Memory: 250.5 MB (93.21%) - LeetSync

--- 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.cpp
+++ 3592-find-x-sum-of-all-k-long-subarrays-ii/find-x-sum-of-all-k-long-subarrays-ii.cpp
@@ -1,60 +1,101 @@
-#define ll long long
-#include <unordered_map>
-#include <vector>
-#include <set>
-using namespace std;
 class Solution {
 public:
-    vector<long long> findXSum(vector<int>& nums, int k, int x) {
-        int n = nums.size();
-        unordered_map<ll, ll> freq; // Frequency map
-        set<pair<ll, ll>> left, right; // Two sets to store pairs of (frequency, value)
-        ll sum = 0;
-        vector<ll> ans;
-        // Add an element to the 'right' set and adjust the sum
-        auto add_right = [&](ll count, ll value) {
-            right.insert({count, value});
-            sum += count * value;
-            if (right.size() > x) {
-                auto smallest = *right.begin(); // Get the smallest element
-                sum -= smallest.first * smallest.second;
-                left.insert(smallest);
-                right.erase(right.begin());
-            }
-        };
-        // Remove an element from the 'right' set and adjust the sum
-        auto remove_right = [&](ll count, ll value) {
-            auto current = make_pair(count, value);
-            if (right.find(current) != right.end()) {
-                sum -= count * value;
-                right.erase(current);
-                if (left.size() && right.size() < x) {
-                    auto largest = *left.rbegin(); // Get the largest element from 'left'
-                    sum += largest.first * largest.second;
-                    right.insert(largest);
-                    left.erase(largest);
-                }
-            } else {
-                left.erase(current); // Erase from 'left' if it's not in 'right'
-            }
-        };
-        int i = 0; // Sliding window start pointer
-        for (int j = 0; j < n; j++) {
-            if (freq[nums[j]] > 0) remove_right(freq[nums[j]], nums[j]);
-            freq[nums[j]]++;
-            add_right(freq[nums[j]], nums[j]);
-            // Maintain the sliding window size of 'k'
-            if (j - i + 1 > k) {
-                remove_right(freq[nums[i]], nums[i]);
-                freq[nums[i]]--;
-                if (freq[nums[i]] > 0) {
-                    add_right(freq[nums[i]], nums[i]);
-                }
-                i++;
-            }
-            // When window size reaches exactly 'k', record the sum
-            if (j - i + 1 == k) ans.push_back(sum);
-        }
-        return ans;
+  vector<long long> findXSum(vector<int>& nums, int k, int x)
+  {
+    auto res = vector<long long>{};
+    auto top = set<array<int, 2>, std::greater<array<int, 2>>>{};
+    auto bot = set<array<int, 2>, std::greater<array<int, 2>>>{};
+    
+    auto sum = 0ll;
+    auto index = 0;
+    auto table = unordered_map<int, int>{};
+    for(; index < k; ++index) ++table[nums[index]];
+    for(const auto& [k, f] : table) 
+    {
+      top.insert({f, k});
+      sum += 1ll * f * k;
     }
+    while(size(top) > x)
+    {
+      auto it = end(top);
+      it = prev(it);
+      sum -= 1ll * (*it)[0] * (*it)[1];
+      bot.insert(*it);
+      top.erase(it);
+    }
+    res.push_back(sum);
+    
+    for(; index < size(nums); ++index)
+    {
+      auto ori = array{table[nums[index - k]], nums[index - k]};
+      if(auto it = top.find(ori); it != cend(top))
+      {
+        top.erase(ori);
+        sum -= 1ll * ori[0] * ori[1];
+      }
+      if(auto it = bot.find(ori); it != cend(bot)) bot.erase(ori);
+      
+      ori = array{table[nums[index]], nums[index]};
+      if(auto it = top.find(ori); it != cend(top))
+      {
+        top.erase(ori);
+        sum -= 1ll * ori[0] * ori[1];
+      }
+      if(auto it = bot.find(ori); it != cend(bot)) bot.erase(ori);
+      
+      if(--table[nums[index - k]] > 0)
+      {
+        auto val = array{table[nums[index - k]], nums[index - k]};
+        top.insert(val);
+        sum += 1ll * val[0] * val[1];
+      }
+      
+      if(++table[nums[index]] > 0)
+      {
+        if(nums[index - k] == nums[index])
+        {
+          auto val = array{table[nums[index]] - 1, nums[index]};
+          top.erase(val);
+          sum -= 1ll * val[0] * val[1];
+        }
+        auto val = array{table[nums[index]], nums[index]};
+        top.insert(val);
+        sum += 1ll * val[0] * val[1];
+      }
+      
+      while(size(top) < x && size(bot) > 0)
+      {
+        auto it = begin(bot);
+        sum += 1ll * (*it)[0] * (*it)[1];
+        top.insert(*it);
+        bot.erase(it);
+      }
+      
+      while(size(top) > x)
+      {
+        auto it = end(top);
+        it = prev(it);
+        sum -= 1ll * (*it)[0] * (*it)[1];
+        bot.insert(*it);
+        top.erase(it);
+      }
+      
+      auto tIt = end(top);
+      tIt = prev(tIt);
+      while(size(bot) > 0 && *(cbegin(bot)) > *tIt)
+      {
+        auto it = begin(bot);
+        sum -= 1ll * (*tIt)[0] * (*tIt)[1];
+        sum += 1ll * (*it)[0] * (*it)[1];
+        top.insert(*it);
+        bot.insert(*tIt);
+        top.erase(tIt);
+        bot.erase(it);
+        tIt = end(top);
+        tIt = prev(tIt);
+      }
+      res.push_back(sum);
+    }
+    return res;
+  }
 };
