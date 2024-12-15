bfe93bec4ca92d56617b7104e863d5921082444f
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-15 12:10:23 +1100

    Time: 295 ms (88.89%) | Memory: 108.8 MB (17.29%) - LeetSync

--- 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.cpp
@@ -1,45 +1,34 @@
 
+
 class Solution {
 public:
     double maxAverageRatio(std::vector<std::vector<int>>& classes, int extraStudents) {
         int n = classes.size();
-        double totalRatio = 0.0;
-
-        // Priority queue to store the gain of adding an extra student (max-heap)
-        auto gain = [](const std::pair<double, int>& a, const std::pair<double, int>& b) {
-            return a.first < b.first; // Max-heap: larger gain comes first
+        double rt = 0;
+        // Define a max heap with custom comparator
+        auto cmp = [](const std::pair<double, int>& a, const std::pair<double, int>& b) {
+            return a.first < b.first; // Max heap based on the first element
         };
-        std::priority_queue<std::pair<double, int>, std::vector<std::pair<double, int>>, decltype(gain)> pq(gain);
+        std::priority_queue<std::pair<double, int>, std::vector<std::pair<double, int>>, decltype(cmp)> maxHeap(cmp);
 
-        // Calculate initial total ratio and populate priority queue
-        for (int i = 0; i < n; ++i) {
-            int pass = classes[i][0], total = classes[i][1];
-            double currentRatio = double(pass) / total;
-            double gainWithExtra = double(pass + 1) / (total + 1) - currentRatio;
-            totalRatio += currentRatio;
-            pq.emplace(gainWithExtra, i);
+        for (int i = 0; i < n; i++) {
+            vector<int> temp = classes[i];
+            rt += double(temp[0]) / temp[1];
+            // Calculate the improvement and push it to the priority queue
+            maxHeap.push({double(temp[1] - temp[0]) / (temp[1] + 1) / temp[1], i});
         }
 
-        // Distribute extra students
         while (extraStudents--) {
-            auto [maxGain, index] = pq.top();
-            pq.pop();
+            auto maxp = maxHeap.top();
+            maxHeap.pop();
+            rt += maxp.first;
 
-            // Update the class with the extra student
-            int& pass = classes[index][0];
-            int& total = classes[index][1];
-            pass++;
-            total++;
-
-            // Recalculate gain for the updated class
-            double newRatio = double(pass) / total;
-            double newGain = double(pass + 1) / (total + 1) - newRatio;
-
-            // Update total ratio and push new gain to the priority queue
-            totalRatio += maxGain;
-            pq.emplace(newGain, index);
+            // Update the class size
+            int b = classes[maxp.second][1]++;
+            // Calculate the updated ratio and push it back to the priority queue
+            maxHeap.push({double(maxp.first) * b / (b + 2), maxp.second});
         }
 
-        return totalRatio / n;
+        return rt / n;
     }
 };
