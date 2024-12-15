26d2cb3f47a458ee8c2678ac97dd73b38e09a5bf
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-15 12:46:47 +1100

    Time: 670 ms (27.57%) | Memory: 291.5 MB (29.11%) - LeetSync

--- 3646-sum-of-good-subsequences/sum-of-good-subsequences.cpp
+++ 3646-sum-of-good-subsequences/sum-of-good-subsequences.cpp
@@ -1,32 +1,55 @@
 class Solution {
 public:
-    int mod = 1000000007;
+    typedef long long ll;
+    const int mod = 1000000007;
 
     int sumOfGoodSubsequences(vector<int>& nums) {
-        map<int, int> cnt, sum;
-        
-        // Loop through all numbers in the nums array
-        for (int i : nums) {
-            // Update cnt[i] by considering subsequences from i-1, i, and i+1
-            cnt[i] += (cnt[i - 1] + cnt[i + 1] + 1) % mod;
-            cnt[i] %= mod;
-            
-            // Update sum[i] by considering subsequences from i-1, i, and i+1
-            sum[i] += (sum[i - 1] + sum[i + 1]) % mod;
-            sum[i] %= mod;
-            
-            // Add the contribution of the subsequences that end at i
-            sum[i] += (long long)(cnt[i - 1] + cnt[i + 1] + 1) % mod * i % mod;
-            sum[i] %= mod;
-        }
-        
-        // Calculate the final result by summing all the values in sum
-        int res = 0;
-        for (auto &p : sum) {
-            res += p.second;
-            res %= mod;
-        }
-        
-        return res;
+         int n = nums.size();
+         unordered_map<ll, ll> mp;
+         unordered_map<ll, ll> index1;
+         mp[nums[0]] = 1;
+         ll ans = 0;
+
+         for (int i = 1; i < n; i++) {
+              if (mp.find(nums[i] - 1) != mp.end()) {
+                 index1[i] = (index1[i] + mp[nums[i] - 1]) % mod;
+              }
+              if (mp.find(nums[i] + 1) != mp.end()) {
+                 index1[i] = (index1[i] + mp[nums[i] + 1]) % mod;
+              }
+              mp[nums[i]] = (mp[nums[i]] + 1 + index1[i]) % mod;
+         }
+
+         unordered_map<int, int> mp1;
+         unordered_map<int, ll> index2;
+         mp1[nums[n - 1]] = 1;
+
+         for (int i = n - 2; i >= 0; i--) {
+            if (mp1.find(nums[i] - 1) != mp1.end()) {
+                 index2[i] = (index2[i] + mp1[nums[i] - 1]) % mod;
+              }
+              if (mp1.find(nums[i] + 1) != mp1.end()) {
+                index2[i] = (index2[i] + mp1[nums[i] + 1]) % mod;
+              }
+              mp1[nums[i]] = (mp1[nums[i]] + 1 + index2[i]) % mod;
+         }
+
+         for (int i = 0; i < n; i++) {
+            ll xx = 0;
+
+            if (index1.find(i) != index1.end() && index2.find(i) != index2.end()) {
+                xx = (xx + nums[i] * index1[i] % mod * index2[i] % mod) % mod;
+            }
+            if (index1.find(i) != index1.end()) {
+                xx = (xx + nums[i] * index1[i] % mod) % mod;
+            }
+            if (index2.find(i) != index2.end()) {
+                xx = (xx + nums[i] * index2[i] % mod) % mod;
+            }
+            xx = (xx + nums[i]) % mod;
+            ans = (ans + xx) % mod;
+         }
+
+         return ans;
     }
 };
