4af53ae06ab6aed80fd9a346e669629ac890e585
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-15 16:15:36 +1100

    Time: 503 ms (84.62%) | Memory: 390 MB (23.08%) - LeetSync

--- 3686-count-beautiful-splits-in-an-array/count-beautiful-splits-in-an-array.cpp
+++ 3686-count-beautiful-splits-in-an-array/count-beautiful-splits-in-an-array.cpp
@@ -3,18 +3,20 @@ public:
 
     int beautifulSplits(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> check(n, vector<int>(n, 0));
+
+        vector<vector<int>> lcps(n, vector<int>(n, 0));
+        // lcps[i][j] = length of the longest common prefix between nums[i:] and nums[j:]
         for (int x = 0 ; x < n ; ++x) {
-            auto & ch = check[x];
-            int maxi = x, maxr = x;
+            auto & lcp = lcps[x];
+            int max_i = x, max_r = x;
             for (int i = x + 1 ; i < n ; ++i) {
-                if (maxr >= i) {
-                    ch[i] = min(maxr - i + 1, ch[i - maxi + x]);
+                if (max_r >= i) {
+                    lcp[i] = min(max_r - i + 1, lcp[i - max_i + x]);
                 }
-                while (i + ch[i] < n && nums[i + ch[i]] == nums[ch[i] + x]) ++ch[i];
-                if (i + ch[i] - 1 > maxr) {
-                    maxi = i;
-                    maxr = i + ch[i] - 1;
+                while (i + lcp[i] < n && nums[i + lcp[i]] == nums[lcp[i] + x]) ++lcp[i];
+                if (i + lcp[i] - 1 > max_r) {
+                    max_i = i;
+                    max_r = i + lcp[i] - 1;
                 }
             }
         }
@@ -23,11 +25,14 @@ public:
         int ans = 0;
         for (int i = 0 ; i < n ; ++i) {
             for (int j = i + 1 ; j < n - 1 ; ++j) {
-                if (check[0][i + 1] >= i + 1 && j - i >= i + 1 ) {                
+                // split: [0,i], [i+1,j], [j+1,n-1]
+                // check if prefix is satisfied
+
+                if (lcps[0][i + 1] >= i + 1 && j - i >= i + 1 ) {
                     ++ans;
                     continue ;
                 }
-                if (check[i+1][j+1] >= j - i) {                
+                if (lcps[i+1][j+1] >= j - i) {                
                     ++ans;
                     continue ;
                 }
