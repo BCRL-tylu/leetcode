1995f9c2ea609f70283e20eb3d190bfb4318b1df
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-17 01:22:27 +1100

    Time: 2 ms (35.42%) | Memory: 27.8 MB (25.16%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
-        std::set<std::pair<int, int>> orderedPairs;
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<>> orderedPairs;
     int n = nums.size();
     // Populate the set
     for (int i = 0; i < n; ++i) {
-        orderedPairs.insert({nums[i], i});
+        orderedPairs.push({nums[i], i});
     }
 
     for (int i =0;i<k;i++){
-        auto temppair = orderedPairs.begin();
-        pair<int, int> holder = {(temppair->first)*multiplier,temppair->second};
-        orderedPairs.erase(temppair);
-        orderedPairs.emplace(holder);
+        auto temppair = orderedPairs.top();
+        orderedPairs.pop();
+        orderedPairs.push({(temppair.first)*multiplier,temppair.second});
     }
 
     vector<int> rt(n);
 
-    for(const auto& pair : orderedPairs){
-        rt[pair.second] = pair.first;
+    for(int i = 0; i < n; ++i){
+        auto temppair = orderedPairs.top();
+        orderedPairs.pop();
+        rt[temppair.second] = temppair.first;
     }
     return rt;
-
     }
 };
