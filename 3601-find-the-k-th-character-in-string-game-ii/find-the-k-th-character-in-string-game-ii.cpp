0f37fdb760802bc935acb25f42e6166c0e475773
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-17 11:24:01 +1100

    Time: 1 ms (15.21%) | Memory: 30 MB (6.01%) - LeetSync

---
+++ 3601-find-the-k-th-character-in-string-game-ii/find-the-k-th-character-in-string-game-ii.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    std::string shiftString(const std::string& input) {
+        std::string result = input;
+        for (char& ch : result) {
+            ch = (ch == 'z') ? 'a' : ch + 1; // Wrap lowercase z to a
+            // Non-alphabet characters remain unchanged
+        }
+        return result;
+    }
+ 
+    char kthCharacter(long long k, vector<int>& operations) {
+        int n = operations.size();
+        n = min(n,47);
+        long long cp = std::pow(2, n);
+        long long k_pos = k;
+        string cs = "a";
+        int set = std::min(n,7);
+        for (int i = 0; i < set; i++) {
+            if (operations[i]) {
+                cs += shiftString(cs);
+            } else {
+                cs += cs;
+            }
+        }
+
+         std::stack<int> do_operations;
+        long long k_prev = k_pos;
+        while (cp > 128) {
+            cp /= 2;
+            k_pos %= cp;
+            if(k_pos ==0){
+                k_pos = cp;
+            }
+            if(k_pos!=k_prev){
+                k_prev = k_pos;
+                do_operations.push(1);// meaning this operation requires to do
+            }else{
+                do_operations.push(0);
+            }
+        }
+
+        char rt = cs[k_pos-1];
+
+        for (int i = set; i < n; i++) {
+            int do_oper = do_operations.top();
+            do_operations.pop();
+            if (operations[i] && do_oper) {
+                rt = (rt == 'z') ? 'a' : rt + 1;
+            } else {
+                rt = rt;
+            }
+        }
+        return rt;
+    }
+};
