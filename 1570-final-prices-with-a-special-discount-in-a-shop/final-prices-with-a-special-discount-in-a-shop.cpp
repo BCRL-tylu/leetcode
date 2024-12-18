04decf72c72fd1e774fd55ad994f0d6ac1c3a6f0
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-18 17:49:45 +1100

    Time: 0 ms (100.00%) | Memory: 14.2 MB (17.75%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    vector<int> finalPrices(vector<int>& prices) {
+    std::vector<int> finalPrices(std::vector<int>& prices) {
         int n = prices.size();
         std::vector<int> ans = prices;
         std::stack<std::pair<int, int>> stack;
         int i = 0;
-        while(i < n) {
-            // Down the hill
+        while(i < n-1) {
             while (i + 1 < n && ans[i] >= ans[i + 1]) {
-                ans[i] -= ans[i + 1];  // Correct subtraction
-                i++;  // Move i after the subtraction
+                ans[i] -= ans[i + 1];
+                i++;
                 while (!stack.empty()) {
                     auto current = stack.top();
                     if (ans[i] <= current.first) {
@@ -20,12 +19,9 @@ public:
                     }
                 }
             }
-            // Up the hill
             while (i + 1 < n && ans[i + 1] > ans[i]) {
-                stack.push({ans[i], i});
-                i++;
+                stack.push({ans[i], i++});
             }
-            if(i == n-1){break;}
         }
         return ans;
     }
