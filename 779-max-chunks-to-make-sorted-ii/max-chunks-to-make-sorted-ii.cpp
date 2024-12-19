797f89450504fac0ca3d4aa2bbf704abda9d6ef7
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-19 13:31:57 +1100

    Time: 7 ms (20.94%) | Memory: 17.5 MB (15.02%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
@@ -7,15 +7,13 @@ public:
         int seg = 0;
         std::unordered_map<int, int> myMap;
         for (int i = 0; i < n; i++) {
-            if (arr[i] != sortedArr[i]) {
-                if (++myMap[sortedArr[i]] == 0) {
-                    myMap.erase(sortedArr[i]);
-                }
-                if (--myMap[arr[i]] == 0) {
-                    myMap.erase(arr[i]);
-                }
+            if(++myMap[sortedArr[i]]==0){
+                myMap.erase(sortedArr[i]);
             }
-            if (myMap.size() == 0) {
+            if(--myMap[arr[i]]==0){
+                myMap.erase(arr[i]);
+            }
+            if(myMap.empty()){
                 seg++;
             }
         }
