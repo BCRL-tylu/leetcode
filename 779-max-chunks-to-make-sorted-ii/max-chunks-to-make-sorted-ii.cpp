035449d8aea1a7f7c0d2816138c8f9cb3ebd7718
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-19 12:46:26 +1100

    Time: 4 ms (26.60%) | Memory: 17.5 MB (15.02%) - LeetSync

--- 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
+++ 779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.cpp
@@ -3,13 +3,10 @@ public:
     int maxChunksToSorted(vector<int>& arr) {
         std::vector<int> sortedArr = arr;
         std::sort(sortedArr.begin(), sortedArr.end());
-        std::priority_queue<int> pq;
-        std::priority_queue<int> temp_pq;
         int n = arr.size();
         int seg = 0;
         std::unordered_map<int, int> myMap;
         for (int i = 0; i < n; i++) {
-            pq.push(arr[i]);
             if(++myMap[sortedArr[i]]==0){
                 myMap.erase(sortedArr[i]);
             }
