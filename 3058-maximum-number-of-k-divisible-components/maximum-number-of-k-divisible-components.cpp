af90dbacbb65a930bced50858908ec081b4d1c9a
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-22 00:39:02 +1100

    Time: 93 ms (91.14%) | Memory: 175 MB (75.95%) - LeetSync

--- 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/maximum-number-of-k-divisible-components.cpp
@@ -1,12 +1,41 @@
 #include <vector>
-#include <unordered_map>
 using namespace std;
 
 class Solution {
+private:
+    vector<vector<int>> tree;
+    vector<int> values;
+    int components;
+    int k;
+
+    long long dfs(int node, int parent) {
+        long long subtreeSum = values[node];
+
+        for (int neighbor : tree[node]) {
+            if (neighbor != parent) {
+                long long childSum = dfs(neighbor, node);
+                if (childSum % k == 0) {
+                    // If the child subtree is divisible by k, we can dissociate it
+                    components++;
+                } else {
+                    // Otherwise, add its sum to the current node's sum
+                    subtreeSum += childSum;
+                }
+            }
+        }
+        // If the current node's subtree sum is divisible by k, it contributes a component
+        if (parent == -1 && subtreeSum % k == 0) {
+            components++;
+        }
+        return subtreeSum;
+    }
+
 public:
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        vector<vector<int>> tree(n);
-        vector<int> visited(n, 0);
+        this->tree.assign(n, vector<int>());
+        this->values = values;
+        this->components = 0;
+        this->k = k;
 
         // Build adjacency list for the tree
         for (auto& edge : edges) {
@@ -14,36 +43,8 @@ public:
             tree[edge[1]].push_back(edge[0]);
         }
 
-        int components = 0;
-
-        // Helper DFS function
-        function<long long(int)> dfs = [&](int node) {
-            visited[node] = 1;
-            long long subtreeSum = values[node];
-
-            for (int neighbor : tree[node]) {
-                if (!visited[neighbor]) {
-                    long long childSum = dfs(neighbor);
-                    if (childSum % k == 0) {
-                        // If the child subtree is divisible by k, we can dissociate it
-                        components++;
-                    } else {
-                        // Otherwise, add its sum to the current node's sum
-                        subtreeSum += childSum;
-                    }
-                }
-            }
-
-            return subtreeSum;
-        };
-
-        // Start DFS from node 0 (arbitrary root)
-        long long totalSum = dfs(0);
-
-        // If the entire tree sum is divisible by k, count the root as a valid component
-        if (totalSum % k == 0) {
-            components++;
-        }
+        // Start DFS from node 0 (arbitrary root), with no parent (-1)
+        dfs(0, -1);
 
         return components;
     }
