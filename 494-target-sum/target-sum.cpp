7a9bab3f3c96078077c84b3b0d1648391384033f
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-26 17:49:05 +1100

    Time: 948 ms (8.93%) | Memory: 11.5 MB (78.96%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    private:
+    int dfs(const vector<int>& nums, int target, int index, int currentSum) {
+        if (index == nums.size()) {
+            // Base case: check if the current sum equals the target
+            return currentSum == target ? 1 : 0;
+        }
+        
+        // Recursive case: explore '+' and '-' branches
+        int add = dfs(nums, target, index + 1, currentSum + nums[index]);
+        int subtract = dfs(nums, target, index + 1, currentSum - nums[index]);
+        
+        return add + subtract;
+    }
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        return dfs(nums, target, 0, 0);
+    }
+};
