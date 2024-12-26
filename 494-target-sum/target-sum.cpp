90e6eb886f52be4b8ef138d6ca7a415629734575
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-26 18:13:17 +1100

    Time: 624 ms (27.27%) | Memory: 11.8 MB (64.97%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,28 @@
 class Solution {
     private:
-    int dfs(const vector<int>& nums, int target, int index, int currentSum) {
+    int dfs(const vector<int>& nums,const vector<int>& cumsum, int target, int index, int currentSum) {
         if (index == nums.size()) {
             // Base case: check if the current sum equals the target
             return currentSum == target ? 1 : 0;
+        }else{
+            if(currentSum+cumsum[index]<target){
+                return 0;
+            }
         }
         
         // Recursive case: explore '+' and '-' branches
-        int add = dfs(nums, target, index + 1, currentSum + nums[index]);
-        int subtract = dfs(nums, target, index + 1, currentSum - nums[index]);
+        int add = dfs(nums, cumsum,target, index + 1, currentSum + nums[index]);
+        int subtract = dfs(nums, cumsum, target, index + 1, currentSum - nums[index]);
         return add + subtract;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        return dfs(nums, abs(target), 0, 0);
+        int n = nums.size();
+        vector<int> cumSum(n);
+        cumSum[n-1] = nums[n-1];
+for (int i = n - 2; i >= 0; --i) {
+    cumSum[i] = cumSum[i + 1] + nums[i];
+}
+        return dfs(nums,cumSum, abs(target), 0, 0);
     }
 };
