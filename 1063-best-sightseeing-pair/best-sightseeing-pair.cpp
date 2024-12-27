4cf67dd9ea01099585254371a9a029079cfcdab7
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-27 13:49:06 +1100

    Time: 0 ms (100.00%) | Memory: 45.6 MB (55.58%) - LeetSync

--- 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/best-sightseeing-pair.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int maxScoreSightseeingPair(vector<int>& values) {
-        int maxRight = values.back() - (values.size() - 1);
-        int maxScore = INT_MIN;
-        for (int i = values.size() - 2; i >= 0; --i) {
-            maxScore = max(maxScore, values[i] + i + maxRight);
-            maxRight = max(maxRight, values[i] - i);
+        
+        int res = 0  , imax =0  ;
+        for(int a : values){
+            res =  max(res ,  imax +  a);
+            imax =  max(imax ,  a )  -1 ; 
         }
-        return maxScore;
+        
+        return res ;
+        
     }
 };
