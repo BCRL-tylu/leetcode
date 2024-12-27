2b1244cfb8acd1150c965adefa0d65eda4ffff55
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-27 20:17:35 +1100

    Time: 12 ms (100.00%) | Memory: 50.5 MB (100.00%) - LeetSync

--- 3740-maximum-number-of-matching-indices-after-right-shifts/maximum-number-of-matching-indices-after-right-shifts.cpp
+++ 3740-maximum-number-of-matching-indices-after-right-shifts/maximum-number-of-matching-indices-after-right-shifts.cpp
@@ -11,18 +11,17 @@ public:
 
         // Step 2: Initialize the distance vector
         vector<int> distance_vector(n, 0);
-
+        int m = 0;
         // Step 3: Calculate the right shift distances
         for (int i = 0; i < n; ++i) {
             if (hm.count(nums1[i])) {
                 for (int idx : hm[nums1[i]]) {
                     int distance = (idx - i + n) % n; // Right shift distance
-                    distance_vector[distance]++;
+                    m = max(m,++distance_vector[distance]);
                 }
             }
         }
-
         // Step 4: Return the maximum value in distance vector
-        return *max_element(distance_vector.begin(), distance_vector.end());
+        return m;
     }
 };
