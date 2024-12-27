43b02e153d258425073a14ad91a9d7ad9802f719
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-27 20:42:02 +1100

    Time: 16 ms (100.00%) | Memory: 50.4 MB (100.00%) - LeetSync

--- 3740-maximum-number-of-matching-indices-after-right-shifts/maximum-number-of-matching-indices-after-right-shifts.cpp
+++ 3740-maximum-number-of-matching-indices-after-right-shifts/maximum-number-of-matching-indices-after-right-shifts.cpp
@@ -3,12 +3,10 @@ public:
     int maximumMatchingIndices(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         unordered_map<int, vector<int>> hm;
-
         // Build the hashmap for nums2
         for (int i = 0; i < n; ++i) {
             hm[nums2[i]].emplace_back(i);
         }
-
         // Track distances and find the maximum
         vector<int> distance_vector(n, 0);
         int max_matches = 0;
