182e9e529b2fd18dad38c9e262703aa19dc8bb63
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-29 20:21:38 +1100

    Time: 162 ms (100.00%) | Memory: 26.7 MB (100.00%) - LeetSync

--- 3699-count-special-subsequences/count-special-subsequences.cpp
+++ 3699-count-special-subsequences/count-special-subsequences.cpp
@@ -1,22 +1,19 @@
+int cnt[1001][1001];
 class Solution {
 public:
-    long long numberOfSubsequences(vector<int>& nums) {
-        vector<vector<double>> sr(nums.size(), vector<double>(nums.size()));
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = i + 2; j < nums.size(); j++) {
-                sr[i][j] = (double)nums[j] / nums[i];
-            }
+long long numberOfSubsequences(vector<int>& n) {
+    long long res = 0;
+    memset(cnt, 0, sizeof(cnt));
+    for (int q = n.size() - 5; q > 1; --q) {
+        for (int s = q + 4, r = q + 2; s < n.size(); ++s) {
+            int f = gcd(n[r], n[s]);
+            ++cnt[n[r] / f][n[s] / f];
+        }                   
+        for (int p = 0; p + 1 < q; ++p) {
+            int f = gcd(n[p], n[q]);
+            res += cnt[n[q] / f][n[p] / f];
         }
-        unordered_map<double, int> mp;
-        long long ans = 0;
-        for (int i = nums.size() - 5; i >= 0; i--) {
-            for (int j = 0; j < nums.size(); j++) {
-                mp[sr[i+2][j]]++;
-            }
-            for (int j = i - 2 ; j>=0; j--) {
-                ans += mp[(double)nums[j] / nums[i]];
-            }
-        }
-        return ans;
     }
+    return res;
+}
 };
