779e579ecee4093a2e5afb65c70672b6fbcd5f11
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-31 01:10:24 +1100

    Time: 4 ms (70.11%) | Memory: 13.8 MB (61.61%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -4,18 +4,22 @@ public:
         if(numRows == 1){
             return s;
         }
+        
         std::vector<string> vec(numRows);
         int n = s.size();
-        int series = 2*numRows-2;
+        int k =numRows-1;
+        int series = 2*k;
+
         for(int i = 0; i<n;i++){
             int cp = i % series;
             char e = s[i];
-            if(cp<= numRows-1){
+            if(cp<= k){
                 vec[cp]+=e;
             }else{
                 vec[series-cp]+=e;
             }
         }
+
         std::string result;
         for (const auto& str : vec) {
             result += str; // Append each string to the result
