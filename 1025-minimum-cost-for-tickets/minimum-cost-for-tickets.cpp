1273cb71eb7865eda85036a57d5b4b991a99c42c
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-01 01:08:49 +1100

    Time: 2 ms (31.67%) | Memory: 14 MB (15.24%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,8 +1,3 @@
-#include <vector>
-#include <queue>
-#include <algorithm>
-#include <limits.h>
-
 class Solution {
 public:
     int mincostTickets(std::vector<int>& days, std::vector<int>& costs) {
@@ -11,30 +6,24 @@ public:
         int n = days.size();
         std::vector<int> dp(n + 1, INT_MAX);
         dp[0] = 0;
-
         for (int i = 0; i < n; ++i) {
             // Remove expired entries for 7-day pass
             while (!day7.empty() && day7.front().second < days[i]) {
                 day7.pop();
             }
-
             // Remove expired entries for 30-day pass
             while (!day30.empty() && day30.front().second < days[i]) {
                 day30.pop();
             }
-
             // Calculate minimum cost for the current day
             int oneDayCost = dp[i] + costs[0];
             int sevenDayCost = (!day7.empty()) ? dp[day7.front().first - 1] + costs[1] : dp[i] + costs[1];
             int thirtyDayCost = (!day30.empty()) ? dp[day30.front().first - 1] + costs[2] :dp[i] + costs[2];
-
             dp[i + 1] = std::min({oneDayCost, sevenDayCost, thirtyDayCost});
-
             // Add the current day to the queues for 7-day and 30-day passes
             day7.push({i + 1, days[i] + 6});
             day30.push({i + 1, days[i] + 29});
         }
-
         return dp[n];
     }
 };
