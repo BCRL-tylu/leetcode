656e82eb3157df105081de3137814293f9bc42eb
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2024-12-31 17:15:13 +1100

    Time: 3 ms (26.60%) | Memory: 14 MB (15.24%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.cpp
@@ -1,16 +1,15 @@
 #include <vector>
 #include <queue>
-#include <utility>
 #include <algorithm>
 #include <limits.h>
 
 class Solution {
 public:
     int mincostTickets(std::vector<int>& days, std::vector<int>& costs) {
-        queue<pair<int, int>> day7;  // Queue for 7-day pass
-        queue<pair<int, int>> day30; // Queue for 30-day pass
+        std::queue<std::pair<int, int>> day7;  // Queue for 7-day pass
+        std::queue<std::pair<int, int>> day30; // Queue for 30-day pass
         int n = days.size();
-        vector<int> dp(n + 1, 0);
+        std::vector<int> dp(n + 1, INT_MAX);
         dp[0] = 0;
 
         for (int i = 0; i < n; ++i) {
@@ -18,30 +17,24 @@ public:
             while (!day7.empty() && day7.front().second < days[i]) {
                 day7.pop();
             }
-            int oneday = min(dp[i] + costs[0],dp[i] + costs[1]);
-            oneday = min(oneday,dp[i] + costs[2]);
-
-            if (!day7.empty()) {
-                dp[i + 1] = min(oneday, dp[day7.front().first - 1] + costs[1]);
-            } else {
-                dp[i + 1] = oneday;
-            }
 
             // Remove expired entries for 30-day pass
             while (!day30.empty() && day30.front().second < days[i]) {
                 day30.pop();
             }
-            if (!day30.empty()) {
-                dp[i + 1] = min(dp[i + 1], dp[day30.front().first - 1] + costs[2]);
-            } else {
-                dp[i + 1] = min(oneday, dp[i + 1]);
-            }
 
-            // Push current day with valid range for 7-day and 30-day passes
-            day7.push({i + 1, days[i] + 6});  // 7-day pass expires after 6 days
-            day30.push({i + 1, days[i] + 29}); // 30-day pass expires after 29 days
+            // Calculate minimum cost for the current day
+            int oneDayCost = dp[i] + costs[0];
+            int sevenDayCost = (!day7.empty()) ? dp[day7.front().first - 1] + costs[1] : dp[i] + costs[1];
+            int thirtyDayCost = (!day30.empty()) ? dp[day30.front().first - 1] + costs[2] :dp[i] +  costs[2];
+
+            dp[i + 1] = std::min({oneDayCost, sevenDayCost, thirtyDayCost});
+
+            // Add the current day to the queues for 7-day and 30-day passes
+            day7.push({i + 1, days[i] + 6});
+            day30.push({i + 1, days[i] + 29});
         }
 
-        return dp[n];  // Return minimum cost for all days
+        return dp[n];
     }
 };
