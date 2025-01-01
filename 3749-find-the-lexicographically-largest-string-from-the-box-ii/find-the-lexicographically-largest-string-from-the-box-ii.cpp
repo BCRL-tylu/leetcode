3c560a66bd3eadbaff14dc1140e742cfebc282c2
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-01 15:50:02 +1100

    Time: 39 ms (100.00%) | Memory: 36.4 MB (100.00%) - LeetSync

--- 3749-find-the-lexicographically-largest-string-from-the-box-ii/find-the-lexicographically-largest-string-from-the-box-ii.cpp
+++ 3749-find-the-lexicographically-largest-string-from-the-box-ii/find-the-lexicographically-largest-string-from-the-box-ii.cpp
@@ -12,21 +12,17 @@ public:
         for (char c : word) {
             maxm = max(maxm, c);
         }
-
         string maxstring = "";  // To keep track of the largest substring
         int start_index = 0;    // To track the starting index of the max substring
-
         // Traverse the string to find substrings starting with maxm
         for (int i = 0; i < n; i++) {
             if (word[i] == maxm) {
                 string temp_ans = "";
                 temp_ans += maxm;
-
                 // Collect characters while the conditions hold
                 while (i + 1 < n && temp_ans.size() < min(k, n - i) && (word[i + 1] != maxm || temp_ans.back() == maxm)) {
                     temp_ans += word[++i];
                 }
-
                 // Compare temp_ans with current largest substring
                 if (temp_ans > maxstring) {
                     maxstring = temp_ans; // Update the largest substring
@@ -34,13 +30,10 @@ public:
                 }
             }
         }
-
         // Build the answer based on the largest substring found
         int sz = maxstring.size();
         string temp_add = word.substr(start_index + 1, min(k - sz, n - start_index));
-
         ans = maxstring + temp_add; // Combine the largest substring with the additional characters
-
         return ans.substr(0, k); // Return the result trimmed to size k
     }
 };
