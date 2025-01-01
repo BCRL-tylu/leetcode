80271cbea8ca9f08e992bc407e8c038c39316ee8
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-01 16:25:28 +1100

    Time: 32 ms (100.00%) | Memory: 34.9 MB (100.00%) - LeetSync

--- 3749-find-the-lexicographically-largest-string-from-the-box-ii/find-the-lexicographically-largest-string-from-the-box-ii.cpp
+++ 3749-find-the-lexicographically-largest-string-from-the-box-ii/find-the-lexicographically-largest-string-from-the-box-ii.cpp
@@ -1,48 +1,45 @@
-#include <string>
-#include <algorithm>
-
 class Solution {
 public:
-    std::string answerString(std::string word, int f) {
+    string answerString(string word, int f) {
         if (f == 1) return word;
 
-        char maxChar = 'a'; // Maximum character in the word
-        int length = word.size();
-        int subLength = length - f + 1; // Length of the substring to be formed
-        std::string result = "";
+        char mc = 'a'; // Maximum character in the word
+        int len = word.size();
+        int sl = len - f + 1; // Length of the longest substring to be formed
+        string res = "";
 
         // Find the maximum character in the word
         for (char ch : word) {
-            maxChar = std::max(maxChar, ch);
+            mc = max(mc, ch);
         }
 
-        std::string largestSub = "";  // To keep track of the largest substring
-        int largestSubEnd = 0;        // To track the ending index of the largest substring
+        string ls = "";  // Largest substring
+        int lse = 0;     // Ending index of the largest substring
 
-        // Traverse the string to find substrings starting with maxChar
-        for (int i = 0; i < length; i++) {
-            if (word[i] == maxChar) {
-                std::string currentSub = "";
-                currentSub += maxChar;
+        // Traverse the string to find substrings starting with mc
+        for (int i = 0; i < len; i++) {
+            if (word[i] == mc) {
+                string cs = ""; // Current substring
+                cs += mc;
 
                 // Keep appending elements until we reach the limit
-                while (i + 1 < length && currentSub.size() < std::min(subLength, length - i) &&
-                       (word[i + 1] != maxChar || currentSub.back() == maxChar)) {
-                    currentSub += word[++i];
+                // Stop when reaching the boundary of the word, the substring limit, or encountering another mc not connected consecutively
+                while (i + 1 < len && cs.size() < min(sl, len - i) && (word[i + 1] != mc || cs.back() == mc)) {
+                    cs += word[++i];
                 }
 
                 // Update the largest substring if the current one is better
-                if (currentSub > largestSub) {
-                    largestSub = currentSub;
-                    largestSubEnd = i;
+                if (cs > ls) {
+                    ls = cs;
+                    lse = i;
                 }
             }
         }
 
-        int largestSubSize = largestSub.size();
-        std::string remainingSub = word.substr(largestSubEnd + 1, std::min(subLength - largestSubSize, length - largestSubEnd));
-        result = largestSub + remainingSub;
+        int lss = ls.size(); // Largest substring size
+        string rem = word.substr(lse + 1, min(sl - lss, len - lse));
+        res = ls + rem;
 
-        return result; // Return the result trimmed to the desired size
+        return res; // Return the result trimmed to the desired size
     }
 };
