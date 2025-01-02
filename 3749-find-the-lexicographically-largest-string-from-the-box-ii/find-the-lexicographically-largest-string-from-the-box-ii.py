74e1fdab3ea44054fab9f8e74779c11402036b5d
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-02 15:26:49 +1100

    Time: 154 ms (100.00%) | Memory: 20.1 MB (100.00%) - LeetSync

--- 3749-find-the-lexicographically-largest-string-from-the-box-ii/find-the-lexicographically-largest-string-from-the-box-ii.py
+++ 3749-find-the-lexicographically-largest-string-from-the-box-ii/find-the-lexicographically-largest-string-from-the-box-ii.py
@@ -1,21 +1,23 @@
 class Solution:
     def answerString(self, word: str, numFriends: int) -> str:
-        if numFriends == 1:
+        if numFriends==1:
             return word
-        
         n = len(word)
-        maxSize = n - numFriends + 1
-
-        pq = []
+        mx_i,offset = 0,0
+        for i in range(1,n):
+            v,mx_v = word[i],word[mx_i+offset]
+            if v==mx_v:
+                offset+=1
+            elif v<mx_v:
+                offset = 0
+            elif v>mx_v:
+                if word[i-offset]>=word[i]:
+                    mx_i = i-offset
+                else:
+                    mx_i = i
+                offset = 0
+        extra = max(0,numFriends - mx_i - 1)
+        if extra:
+            return word[mx_i:-extra]
+        return word[mx_i:]
         
-        for idx, c in enumerate(word):
-            heapq.heappush(pq, (-ord(c), idx))
-        
-        max_char = -pq[0][0]
-
-        ans = ""
-        while pq and -pq[0][0] == max_char:
-            _, idx = heapq.heappop(pq)
-            ans = max(ans, word[idx:min(idx + maxSize, n + 1)])
-        
-        return ans
