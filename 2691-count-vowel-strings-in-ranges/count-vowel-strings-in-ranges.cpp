7bf6803fb1fb9f1c65d610b11a5f24a8a3c46bcc
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-02 18:13:27 +1100

    Time: 3 ms (89.32%) | Memory: 69.8 MB (63.35%) - LeetSync

--- 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.cpp
@@ -1,26 +1,20 @@
 class Solution {
-private:
-    bool isVowel(char ch) {
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
-    };
-
 public:
-    vector<int> vowelStrings(vector<string>& words,
-                             vector<vector<int>>& queries) {
-        int n = words.size();
-        vector<int> prefixsum(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            if (isVowel(words[i].front()) && isVowel(words[i].back())) {
-                prefixsum[i + 1] = prefixsum[i] + 1;
-            } else {
-                prefixsum[i + 1] = prefixsum[i];
-            }
+    vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
+        int n=words.size();
+        vector <int> isVowel(n,0);
+        for(int i=0;i<n;i++){
+            int l=words[i].size()-1;
+            if((words[i][0]=='a' or words[i][0]=='e' or words[i][0]=='i' or words[i][0]=='o' or words[i][0]=='u') and (words[i][l]=='a' or words[i][l]=='e' or words[i][l]=='i' or words[i][l]=='o' or words[i][l]=='u'))
+                isVowel[i]=1;
         }
-        int k = queries.size();
-        vector<int> ans;
-        for (int j = 0; j < k; j++) {
-            ans.push_back(prefixsum[queries[j][1] + 1] - prefixsum[queries[j][0]]);
+        vector <int> prefix(n+1,0);
+        for(int i=0;i<n;i++)
+            prefix[i+1]=prefix[i]+isVowel[i];
+        vector <int> res(queries.size());
+        for(int i=0;i<queries.size();i++){
+            res[i]=prefix[queries[i][1]+1]-prefix[queries[i][0]];
         }
-        return ans;
+        return res;
     }
 };
