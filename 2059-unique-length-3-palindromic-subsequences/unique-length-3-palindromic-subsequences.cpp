d89086e0b54fd45c9c64fcaad738fb3abb539a41
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-04 19:06:05 +1100

    Time: 55 ms (94.17%) | Memory: 14.6 MB (92.02%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-    std::vector<std::vector<bool>> matrix(26, std::vector<bool>(26,true));
+    std::vector<std::vector<int>> matrix(26, std::vector<int>(26, 0));
     std::vector<int> value(26, 0);
     std::vector<bool> visited(26, false);
     std::vector<int> ans_value(26, 0);
@@ -14,8 +14,8 @@ public:
         // Step (1): Update the matrix and value for all visited letters
         for (int i = 0; i < 26; ++i) {
             if (visited[i]) {
-                if (matrix[i][current]) {
-                    matrix[i][current] = !matrix[i][current];
+                if (matrix[i][current] == 0) {
+                    matrix[i][current] = 1;
                     if(current == i){
                         fm = true;
                     }
@@ -28,17 +28,16 @@ public:
         if (!visited[current]) {
             visited[current] = true;
         } else {
+            // Step (3): If the letter is already visited and not adjacent to an identical char
                 ans_value[current] =value[current];
-                if(fm){
-                    ans_value[current]--;
-                }
+                if(fm){ ans_value[current]--;}
         }
     }
 
     for(int i =0; i<26;i++){
         ans += ans_value[i];
-        
     }
+
     return ans;
     }
 };
