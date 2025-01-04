9860135acf48812118d8c5d23d46ce1a82ad0c22
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-04 23:28:46 +1100

    Time: 0 ms (100.00%) | Memory: 8.9 MB (93.18%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    int myAtoi(string s) {
-        int i = 0;
-        int n = s.size();
-        while (i < n && s[i] == ' ') {
-            i++;
+    int myAtoi(const string& s) {
+        bool isNeg = false;
+        int i = 0, sz = s.size();
+        while (i < sz && s[i] == ' ') ++i;
+
+        if (i >= sz) return 0;
+        if (s[i] == '-') isNeg = true;
+        if (s[i] == '-' || s[i] == '+') ++i;
+
+        while (i < sz && s[i] == '0') ++i;
+
+        int64_t ret = 0, maxValue = (int64_t)INT32_MAX + 1;
+        while (i < sz && '0' <= s[i] && s[i] <= '9'
+                && ret <= maxValue) {
+            ret = ret * 10 + (s[i] - '0');
+            ++i;
         }
-        int sign = 1;
-        if (i < n && s[i] == '-' || s[i] == '+') {
-            sign = (s[i] == '-') ? -1 : 1;
-            i++;
-        }
-        long long no = 0;
-        while (i < n && isdigit(s[i])) {
-            no = no * 10 + s[i] - '0';
-            if (no * sign > INT_MAX) {
-                return INT_MAX;
-            }
-            if (no * sign < INT_MIN) {
-                return INT_MIN;
-            }
-            i++;
-        }
-        return no * sign;
+        if (isNeg) ret = -ret;
+        if (ret < INT32_MIN) return INT32_MIN;
+        if (ret > INT32_MAX) return INT32_MAX;
+        return ret;
     }
 };
