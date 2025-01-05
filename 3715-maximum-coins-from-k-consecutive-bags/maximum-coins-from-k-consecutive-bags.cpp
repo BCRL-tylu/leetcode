6912968e1ba8dba4fe250e6347cadeb6a4623665
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-05 15:08:11 +1100

    Time: 127 ms (100.00%) | Memory: 173.9 MB (77.78%) - LeetSync

--- 3715-maximum-coins-from-k-consecutive-bags/maximum-coins-from-k-consecutive-bags.cpp
+++ 3715-maximum-coins-from-k-consecutive-bags/maximum-coins-from-k-consecutive-bags.cpp
@@ -1,88 +1,28 @@
-#define FOR(i,a,b)   for(int i = a; i < b.size(); ++i)
-#define F(i,a,b)     for(int i = a; i < b; ++i)
-#define F0(i,b)      for(int i = 0; i< b.size();++i)
-#define BE(a)        a.begin(), a.end()
-#define __S(a)       a.size()
-#define pb(a)        push_back(a)
-#define pr(a)        cout << a << " ";
-
-typedef long long ll;
-typedef vector<string> vs;
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-typedef vector <long long> vl;
-typedef vector<vector<long long>> vvl;
-
 class Solution {
 public:
     long long maximumCoins(vector<vector<int>>& coins, int k) {
-        // can only be at the start or the end 
-        // need to find the next number and the previous
-        // bs
-        sort(BE(coins));
-        ll coinPre[100001]; // contain
-        coinPre[0] = 0;
-        F(i, 0, __S(coins)) 
-            coinPre[i+1] = coinPre[i] + (ll)coins[i][2]*(coins[i][1]-coins[i][0]+1);
-
-        ll ans = {};
-
-        // need to,  find the upper bound the biggest [0] <= i
-        auto up = [&](int i){
-            int l = 0, r = __S(coins)-1;
-            int biggest = -1;
-            while (l <= r){
-                int mid = (l+r)/2;
-                if (coins[mid][0] <= i){
-                    biggest = mid;
-                    l = mid+1;
-                }else{
-                    r = mid-1;
-                }
-            }
-            return biggest;
-        };
-        
-        // need to find the lower bound ,find the smallest [1] >= i
-        auto lw = [&](int i){
-            int l = 0, r = __S(coins)-1;
-            int biggest = -1;
-            while (l <= r){
-                int mid = (l+r)/2;
-                if (coins[mid][1] >= i){
-                    biggest = mid;
-                    r = mid -1;
-                }else{
-                    l = mid +1;
-                }
-            }
-            return biggest;
-        };
-
-        F0(i, coins){
-            // from the start +k-1, find the previous one, the biggest [0] <= +k-1
-            int pre = up(coins[i][0]+k-1);
-            //cout << pre << " ";
-            if (coins[i][0]+k-1 >= coins[pre][1]){
-                ans = max(ans, coinPre[pre+1] - coinPre[i]);
-            }else{
-                //take partial
-                ans = max(ans, coinPre[pre+1] - coinPre[i] - (ll)coins[pre][2]*(coins[pre][1] - (coins[i][0]+k-1)));
-            }
-            //cout << ans << endl;
-            // from the end, -k+1, find the smallest [1] >= -k+1
-            pre = lw(coins[i][1]-k+1);
-            //cout << pre << " ";
-            if (coins[i][1]-k+1 <= coins[pre][0]){
-                ans = max(ans, coinPre[i+1] - coinPre[pre]);
-            }else{
-                //take partial
-                ans = max(ans, coinPre[i+1] - coinPre[pre] - (ll)coins[pre][2]*((coins[i][1]-k+1)- coins[pre][0]));
-            }
-            //cout << ans << endl;
+        sort(begin(coins), end(coins));
+        vector<long long> pos;
+        for(auto& c : coins) {
+            int l = c[0], r = c[1];
+            pos.push_back(l);
+            pos.push_back(max(1,r - k + 1));
         }
-        return ans;
-
+        sort(begin(pos), end(pos));
+        pos.erase(unique(begin(pos), end(pos)), end(pos));
+        long long l = 0, r = 0, n = coins.size(), tot = 0, res = 0;
+        for(auto& p : pos) {
+            while(r < n and coins[r][0] <= p + k - 1) {
+                tot += 1ll * (coins[r][1] - coins[r][0] + 1) * coins[r][2];
+                r++;
+            }
+            while(l < r and coins[l][1] < p) {
+                tot -= 1ll * (coins[l][1] - coins[l][0] + 1) * coins[l][2];
+                l++;
+            }
+            res = max(res, tot - max(0ll, coins[r - 1][1] - (p + k - 1)) * coins[r-1][2] - max(0ll, p - coins[l][0]) * coins[l][2]);
+        }
         
+        return res;
     }
 };
