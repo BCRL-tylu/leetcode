273984cd0bbec16824c0960e8ccec4448843205f
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-06 10:49:06 +1100

    Time: 406 ms (56.10%) | Memory: 208.2 MB (21.95%) - LeetSync

--- 3688-maximize-subarray-sum-after-removing-all-occurrences-of-one-element/maximize-subarray-sum-after-removing-all-occurrences-of-one-element.cpp
+++ 3688-maximize-subarray-sum-after-removing-all-occurrences-of-one-element/maximize-subarray-sum-after-removing-all-occurrences-of-one-element.cpp
@@ -1,91 +1,72 @@
+struct node {
+    long long lsum, rsum, sum, m;
+};
+
+struct segmentTree {
+vector<node> tree;
+
+public:
+   segmentTree(int n) : tree(vector<node>(n << 2)) {}
+    
+    long long query(int ind, int left, int right, int x, int y) {
+        if (left >= x && right <= y) {
+            return tree[ind].m;
+        }
+        const int mid = (left + right) >> 1;
+        long long r = LONG_LONG_MIN;
+        if (x <= mid) {
+            r = query(ind << 1, left, mid, x, y);
+        }
+        if (y > mid) {
+            r = max(r, query((ind << 1) | 1, mid + 1, right, x, y));
+        } 
+        return r;
+    }
+
+    void update(int ind, int left, int right, int x, int delta) {
+        if (left >= x && right <= x) {
+            tree[ind].lsum += delta;
+            tree[ind].rsum += delta;
+            tree[ind].sum += delta;
+            tree[ind].m += delta;
+            return;
+        }
+        const int mid = (left + right) >> 1, lson = ind << 1, rson = (ind << 1) | 1;
+        if (x <= mid) {
+            update(lson, left, mid, x, delta);
+        } else {
+            update(rson, mid + 1, right, x, delta);
+        }
+        tree[ind].lsum = max(tree[lson].lsum, tree[lson].sum + tree[rson].lsum);
+        tree[ind].rsum = max(tree[rson].rsum, tree[rson].sum + tree[lson].rsum);
+        tree[ind].sum = tree[lson].sum + tree[rson].sum;
+        tree[ind].m = max(max(tree[lson].m, tree[rson].m), tree[lson].rsum + tree[rson].lsum);
+    }
+};
+
 class Solution {
 public:
-    struct SegmentTree {
-        struct Node {
-            long long totalSum, maxSubarraySum, maxPrefixSum, maxSuffixSum;
-            Node() : totalSum(0), maxSubarraySum(LLONG_MIN), maxPrefixSum(LLONG_MIN), maxSuffixSum(LLONG_MIN) {}
-            Node(long long val) : totalSum(val), maxSubarraySum(val), maxPrefixSum(val), maxSuffixSum(val) {}
-        };
-
-        int n;
-        vector<Node> tree;
-
-        SegmentTree(const vector<int>& nums) {
-            n = nums.size();
-            tree.resize(4 * n);
-            build(nums, 0, 0, n - 1);
-        }
-
-        void build(const vector<int>& nums, int node, int start, int end) {
-            if (start == end) {
-                tree[node] = Node(nums[start]);
-                return;
-            }
-            int mid = (start + end) / 2;
-            build(nums, 2 * node + 1, start, mid);
-            build(nums, 2 * node + 2, mid + 1, end);
-            tree[node] = merge(tree[2 * node + 1], tree[2 * node + 2]);
-        }
-
-        Node merge(const Node& left, const Node& right) {
-            Node result;
-            result.totalSum = left.totalSum + right.totalSum;
-            result.maxPrefixSum = max(left.maxPrefixSum, left.totalSum + right.maxPrefixSum);
-            result.maxSuffixSum = max(right.maxSuffixSum, right.totalSum + left.maxSuffixSum);
-            result.maxSubarraySum = max({left.maxSubarraySum, right.maxSubarraySum, left.maxSuffixSum + right.maxPrefixSum});
-            return result;
-        }
-
-        void update(int idx, int value, int node, int start, int end) {
-            if (start == end) {
-                tree[node] = Node(value);
-                return;
-            }
-            int mid = (start + end) / 2;
-            if (idx <= mid) update(idx, value, 2 * node + 1, start, mid);
-            else update(idx, value, 2 * node + 2, mid + 1, end);
-            tree[node] = merge(tree[2 * node + 1], tree[2 * node + 2]);
-        }
-
-        Node query() {
-            return tree[0];
-        }
-    };
-
     long long maxSubarraySum(vector<int>& nums) {
-        // Check if all numbers are negative
-        long long maxElement = LLONG_MIN;
-        bool allNegative = true;
-        for (int num : nums) {
-            if (num > 0) {
-                allNegative = false;
-            }
-            maxElement = max(maxElement, (long long)num);
-        }
-
-        // If all elements are negative, the result is the largest single element
-        if (allNegative) {
-            return maxElement;
-        }
-
-        unordered_map<int, vector<int>> freq;
-        for (int i = 0; i < nums.size(); ++i) {
-            freq[nums[i]].push_back(i);
-        }
-
-        SegmentTree segTree(nums);
-        long long result = segTree.query().maxSubarraySum;
-
-        for (auto& [key, indices] : freq) {
-            for (int idx : indices) {
-                segTree.update(idx, 0, 0, 0, nums.size() - 1);
-            }
-            result = max(result, segTree.query().maxSubarraySum);
-            for (int idx : indices) {
-                segTree.update(idx, nums[idx], 0, 0, nums.size() - 1);
+        const int n = nums.size();
+        segmentTree t(n);
+        unordered_map<int, vector<int>> have;
+        for (int i = 0; i < n; ++i) {
+            t.update(1, 0, n - 1, i, nums[i]);
+            if (nums[i] < 0) {
+                have[nums[i]].push_back(i);
             }
         }
-
-        return result;
+        long long r = t.query(1, 0, n - 1, 0, n - 1);
+        if (r <= 0) return r;
+        for (const auto& p : have) {
+            for (int i : p.second) {
+                t.update(1, 0, n - 1, i, -p.first);
+            }
+            r = max(r, t.query(1, 0, n - 1, 0, n - 1));
+            for (int i : p.second) {
+                t.update(1, 0, n - 1, i, p.first);
+            }
+        }
+        return r;
     }
 };
