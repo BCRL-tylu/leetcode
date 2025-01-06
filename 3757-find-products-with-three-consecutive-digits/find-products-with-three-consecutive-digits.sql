7489dcc428db60d27716f0862930f4f7d58e43eb
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-06 18:09:31 +1100

    Time: 414 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3757-find-products-with-three-consecutive-digits/find-products-with-three-consecutive-digits.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT product_id, name
+FROM Products
+WHERE name REGEXP '[0-9]{3}[^0-9]' OR name REGEXP '[^0-9][0-9]{3}[^0-9]' OR name REGEXP '[^0-9][0-9]{3}$' ;
