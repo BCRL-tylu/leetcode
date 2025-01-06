85366616612428d156f684b449ebf61aa35a4503
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-06 12:05:09 +1100

    Time: 0 ms (100.00%) | Memory: 12.3 MB (37.74%) - LeetSync

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -5,24 +5,14 @@ public:
         int l1 = 0, r1 = 0, count = 0;
         vector<int> ans(n, 0);
         for (int i = 0; i < n; i++) {
-            if (boxes[i] == '1') {
-                count += i;
-                r1++;
-            }
+            if (boxes[i] == '1') {count += i;r1++;}
         }
-        if (boxes[0] == '1') {
-            l1++;
-            r1--;
-        }
+        if (boxes[0] == '1') {l1++;r1--;}
         ans[0] = count;
-
         for (int i = 1; i < n; i++) {
             count = count + l1 - r1;
             ans[i] = count;
-            if (boxes[i] == '1') {
-                l1++;
-                r1--;
-            }
+            if (boxes[i] == '1') {l1++;r1--;}
         }
         return ans;
     }
