43239fdd34765c01f2370d42843be24f3f06e1e4
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-01-14 23:04:32 +1100

    Time: 6 ms (60.48%) | Memory: 88.4 MB (31.97%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.cpp
@@ -3,10 +3,10 @@ public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
         int cc = 0 ,n=A.size();
         unordered_map<int,int> abrecord;
-        vector<int> ans(n);
+        vector<int> ans;
         for(int i = 0;i<n;i++){
             cc+=(++abrecord[B[i]])/2+ (++abrecord[A[i]])/2;
-            ans[i] = cc;
+            ans.push_back(cc);
         }
         return ans;
     }
