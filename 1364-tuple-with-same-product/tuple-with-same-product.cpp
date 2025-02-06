ecd2aeaf68f35cfa0d4e072ab2c71c6531df7522
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-06 19:59:19 +1100

    Time: 332 ms (39.50%) | Memory: 84.9 MB (67.71%) - LeetSync

---
+++ 1364-tuple-with-same-product/tuple-with-same-product.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    private:
+    long long factorial(int n) {
+    return std::llround(std::tgamma(n + 1)); // Round to nearest integer
+}
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int,int> hm;
+        for(int i =0; i<(int)nums.size();i++){
+            for(int j = i+1;j<(int)nums.size();j++){
+                hm[nums[i]*nums[j]]++;
+            }
+        }
+        int ans = 0;
+        for(auto&[key, value]:hm){
+            if(value>=2){
+                ans+=4*value*(value-1);
+            }
+        }
+        return ans;
+    }
+};
