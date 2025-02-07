179dfe8081a2e1c6a994069fc308ad6f1a50fd20
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-07 13:54:17 +1100

    Time: 115 ms (57.23%) | Memory: 157.5 MB (60.45%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -5,21 +5,22 @@ public:
         vector<int> ans;
         int count = 0;
         for(auto& q:queries){
-            bool k =i_c.count(q[0]);
-            int c = i_c[q[0]];
-            if(k && c == q[1]){
+            int q0 = q[0], q1 = q[1];
+            bool k =i_c.count(q0);
+            int c = i_c[q0];
+            if(k && c == q1){
                 ans.push_back(count);
                 continue;
             }
-            if(!c_i.count(q[1])) count++;
-            c_i[q[1]]++;
+            if(!c_i.count(q1)) count++;
+            c_i[q1]++;
             if(k){
                 if(--c_i[c]==0){
                     c_i.erase(c);
                     count--;
                 }
             }
-            i_c[q[0]] = q[1];
+            i_c[q0] = q1;
             ans.push_back(count);
         }
         return ans;
