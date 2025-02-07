0280bbce49a7d5a2de8d3a9ed8745633dbf7cfe8
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-07 13:47:20 +1100

    Time: 111 ms (68.49%) | Memory: 157.7 MB (49.84%) - LeetSync

--- 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
+++ 3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     vector<int> queryResults(int limit, vector<vector<int>>& queries) {
-        unordered_map<int, int> c_i, i_c;
+        unordered_map<int,int> c_i,i_c;
         vector<int> ans;
         int count = 0;
-
-        for (auto& q : queries) {
-            int idx = q[0], color = q[1];
-
-            auto it = i_c.find(idx);
-            if (it != i_c.end() && it->second == color) {
+        for(auto& q:queries){
+            if(i_c.count(q[0])&& i_c[q[0]] == q[1]){
                 ans.push_back(count);
                 continue;
             }
-
-            if (c_i.find(color) == c_i.end() || c_i[color] == 0) {
+            if(!c_i.count(q[1])){
                 count++;
             }
-            c_i[color]++;
-
-            if (it != i_c.end()) {
-                int prevColor = it->second;
-                if (--c_i[prevColor] == 0) count--;
+            c_i[q[1]]++;
+            if(i_c.count(q[0])){
+                c_i[i_c[q[0]]]--;
+                // 
+                if(c_i[i_c[q[0]]]==0){
+                    c_i.erase(i_c[q[0]]);
+                    count--;
+                }
+                i_c[q[0]] = q[1];
+            }else{
+                i_c[q[0]] = q[1];
             }
-
-            i_c[idx] = color;
             ans.push_back(count);
         }
         return ans;
