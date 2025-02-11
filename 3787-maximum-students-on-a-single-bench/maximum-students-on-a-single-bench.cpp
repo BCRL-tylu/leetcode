55aae7f2c1ca298fc3e0379cbbb4cbc5d032a061
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-11 19:42:33 +1100

    Time: 8 ms (100.00%) | Memory: 44.9 MB (100.00%) - LeetSync

--- 3787-maximum-students-on-a-single-bench/maximum-students-on-a-single-bench.cpp
+++ 3787-maximum-students-on-a-single-bench/maximum-students-on-a-single-bench.cpp
@@ -1,15 +1,14 @@
-
+const int sz =101;
 class Solution {
 public:
     int maxStudentsOnBench(vector<vector<int>>& st) {
         if(st.size()==0) return 0;
-        const int sz =101;
         int hm[sz][sz] = {},k[sz] = {},ans = INT_MIN;
         for(auto& r:st){
             int s = r[0], b =r[1];
             if(hm[s][b])continue;
             hm[s][b]++;
-            ans = std::max(ans,++k[b]);
+            ans = max(ans,++k[b]);
         }
         return ans;
     }
