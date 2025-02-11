be9c4c9b59bb0f9e6dae0935612ef5b8cf6e0fab
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-11 19:44:30 +1100

    Time: 0 ms (100.00%) | Memory: 44.9 MB (100.00%) - LeetSync

--- 3787-maximum-students-on-a-single-bench/maximum-students-on-a-single-bench.cpp
+++ 3787-maximum-students-on-a-single-bench/maximum-students-on-a-single-bench.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     int maxStudentsOnBench(vector<vector<int>>& st) {
         if(st.size()==0) return 0;
-        int hm[sz][sz] = {},k[sz] = {},ans = INT_MIN;
+        short hm[sz][sz] = {},k[sz] = {},ans = INT_MIN;
         for(auto& r:st){
             int s = r[0], b =r[1];
             if(hm[s][b])continue;
