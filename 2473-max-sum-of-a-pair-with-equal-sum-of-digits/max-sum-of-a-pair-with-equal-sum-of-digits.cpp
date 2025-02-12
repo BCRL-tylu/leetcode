b76ceb062d6db677207e44ed1c66ae85eb25d771
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-12 15:04:21 +1100

    Time: 7 ms (94.60%) | Memory: 63.7 MB (95.95%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -3,32 +3,29 @@
 using namespace std;
 
 class Solution {
-private:
-    int sumdigit(int k) {
-        int r = 0;
-        while (k) {
-            r += k % 10;
-            k /= 10;
-        }
-        return r;
-    }
-
 public:
     int maximumSum(vector<int>& nums) {
-        vector<pair<int, int>> hm(82, {INT_MIN, INT_MIN});  // Using fixed-size vector instead of hashmap
+        vector<pair<int, int>> hm(82, {INT_MIN, INT_MIN}); // Using fixed-size vector instead of hashmap
         int ans = -1;
+
         for (int num : nums) {
-            int ind = sumdigit(num);
-            auto& [f, s] = hm[ind];
-            if (num > f) {
-                s = f;
-                f = num;
-            } else if (num > s) {
-                s = num;
-            } else{
-                continue;
+            int k = num;  // Store original number before modifying num
+            int ind = 0;
+
+            while (num) {
+                ind += num % 10;
+                num /= 10;
             }
-            ans = max(ans, f + s);
+
+            auto& [f, s] = hm[ind];
+
+            if (k > f) {
+                s = f;
+                f = k;
+            } else if (k > s) {
+                s = k;
+            }
+                ans = max(ans, f + s);
         }
         return ans;
     }
