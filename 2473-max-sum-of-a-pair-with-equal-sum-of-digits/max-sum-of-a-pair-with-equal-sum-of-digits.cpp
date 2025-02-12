a7a33c265ba6c9a044fa46c8d20661187006b3c5
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-12 14:54:36 +1100

    Time: 7 ms (94.60%) | Memory: 63.7 MB (87.26%) - LeetSync

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -17,19 +17,19 @@ public:
     int maximumSum(vector<int>& nums) {
         vector<pair<int, int>> hm(82, {0, 0});  // Using fixed-size vector instead of hashmap
         int ans = -1;
-
         for (int num : nums) {
             int ind = sumdigit(num);
-            auto& [first, second] = hm[ind];
-
-            if (num > first) {
-                second = first;
-                first = num;
-            } else if (num > second) {
-                second = num;
+            auto& [f, s] = hm[ind];
+            if (num > f) {
+                s = f;
+                f = num;
+            } else if (num > s) {
+                s = num;
+            } else{
+                continue;
             }
-            if (second > 0) {  // Ensure two valid numbers exist
-                ans = max(ans, first + second);
+            if (s > 0) {  // Ensure two valid numbers exist
+                ans = max(ans, f + s);
             }
         }
         return ans;
