a2729730f24e7c1841f7e2a96893300dc61cdbd3
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-13 12:56:20 +1100

    Time: 87 ms (90.19%) | Memory: 91.6 MB (71.88%) - LeetSync

--- 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
+++ 3332-minimum-operations-to-exceed-threshold-value-ii/minimum-operations-to-exceed-threshold-value-ii.cpp
@@ -4,13 +4,11 @@ public:
         priority_queue<int, vector<int>, greater<int>> pq;
         int op =0;
         for(int i =0; i<(int)nums.size();i++){
-            if(nums[i]<k)pq.push(nums[i]);   
+            if(nums[i]<k) pq.push(nums[i]);   
         }
         while(pq.size()>1){
-            int x = pq.top();
-            pq.pop();
-            int y = pq.top();
-            pq.pop();
+            int x = pq.top(); pq.pop();
+            int y = pq.top(); pq.pop();
             long long toadd = (long long)x*2+y;
             if(toadd<k)pq.push(toadd);
             op++;
