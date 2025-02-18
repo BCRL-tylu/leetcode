2f63ac99b9f97601bb144ae167458aa9f85e95e8
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-18 13:21:45 +1100

    Time: 0 ms (100.00%) | Memory: 7.7 MB (85.60%) - LeetSync

--- 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/construct-smallest-number-from-di-string.cpp
@@ -3,15 +3,12 @@ public:
     string smallestNumber(string pattern) {
         string ans;
         int n = pattern.size(), r;
-        for (int i = 1; i <= n + 1; i++)
-            ans += i + '0';
-        
+        for (int i = 1; i <= n + 1; i++) ans += i + '0';
         for (int i = 0; i < n; i++) {
             if (pattern[i] == 'D') {
                 int l = i;
                 while (i < n && pattern[++i] == 'D') {}
                 reverse(ans.begin()+l,ans.begin()+i+1);
-                if(i ==n-1) break;
                 i--;
             }
         }
