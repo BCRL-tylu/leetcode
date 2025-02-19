ac272ccbdd850122fe3c3e65fa68e6b0fe6df762
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-19 20:33:41 +1100

    Time: 15 ms (76.10%) | Memory: 16.8 MB (74.15%) - LeetSync

--- 3410-find-longest-self-contained-substring/find-longest-self-contained-substring.cpp
+++ 3410-find-longest-self-contained-substring/find-longest-self-contained-substring.cpp
@@ -1,28 +1,31 @@
+
 class Solution {
 public:
     int maxSubstringLength(string s) {
+        // greedy
+        vector<int> l(26, INT_MAX), r(26, -1), c(26, 0);
         int n = s.size();
-        vector<int> l(26, INT_MAX), r(26, -1);
-        
-        // Precompute the first and last occurrence of each letter.
         for (int i = 0; i < n; i++) {
             int id = s[i] - 'a';
             l[id] = min(l[id], i);
             r[id] = max(r[id], i);
         }
-        
         int ans = -1;
-       for (int i = 0; i < 26; i++) {
-            // no such letter next
-            if (l[i] == INT_MAX)  continue;
-            int begin = l[i], last = r[s[begin]-'a'];
-
-            for (int j = begin; j < n; j++) {
-                char x = s[j]-'a';
-                if (l[x] < begin) break; // X-begin-X-last, cannot have begin as begining of the substring
-
-                last = max(last, r[x]); // begin-X-last-X or begin-X--X-last
-                if (j == last && !(begin == 0 && j == n - 1)) ans = max(ans, j - begin + 1);
+        for (int i = 0; i < 26; i++) {
+            if (l[i] == INT_MAX)
+                continue;
+            int m = l[i], v = r[s[m] - 'a'];
+            bool updated = true;
+            for (int t = m; t < n; t++) {
+                char ct = s[t] - 'a';
+                if (l[ct] < m) {
+                    updated = false;
+                    break;
+                }
+                v = max(v, r[ct]);
+                if (v == t && !(m == 0 && v == n - 1)) {
+                    ans = max(ans, v - m + 1);
+                }
             }
         }
         return ans;
