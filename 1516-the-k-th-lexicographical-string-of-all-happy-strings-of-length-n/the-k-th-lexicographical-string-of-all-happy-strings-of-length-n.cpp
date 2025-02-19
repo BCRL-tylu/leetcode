ac77bab87b3cd809a05cd7b0fcc64483ca95be7e
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-19 12:09:03 +1100

    Time: 0 ms (100.00%) | Memory: 8.1 MB (87.05%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,23 +1,25 @@
 char ap[3] = {'a', 'b', 'c'};
-const unordered_map<char, vector<char>> m = {
-    {'a', {'b', 'c'}}, {'b', {'a', 'c'}}, {'c', {'a', 'b'}}};
+const std::unordered_map<char, std::vector<char>> m = {
+    {'a', {'b', 'c'}},
+    {'b', {'a', 'c'}},
+    {'c', {'a', 'b'}}
+};
 
 class Solution {
 public:
     string getHappyString(int n, int k) {
         int ini = 1 << (n - 1);
-        if (k > 3 * ini)
-            return ""; // Fix the boundary check
-
+        if (k > (ini)*3) return "";
         string ans;
-        int ind = (k - 1) / ini;
+
+        int ind = (k-1) / (ini);
         ans.push_back(ap[ind]);
-        k -= ind * ini; // Adjust k for the remaining sequence
+        k -= ind * ini;
 
         for (int i = n - 1; i >= 1; i--) {
-            ini /= 2;
-            ind = (k - 1) / ini;         // Ensure ind remains valid
-            k -= ind * ini;              // Adjust k
+            ini/=2;
+            ind = (k-1) / ini;
+            k -= ind * ini;
             ans.push_back(m.at(ans.back())[ind]);
         }
         return ans;
