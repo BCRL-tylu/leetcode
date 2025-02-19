a404513e6f0af5c22323098fcbac1b1b959a8e9a
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-19 13:20:53 +1100

    Time: 0 ms (100.00%) | Memory: 8.1 MB (87.05%) - LeetSync

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -8,7 +8,7 @@ const unordered_map<char, vector<char>> m = {
 class Solution {
 public:
     string getHappyString(int n, int k) {
-        int ini = 1 << (n - 1);
+        int ini = 1 << (--n);
         if (k > (ini)*3) return "";
         string ans;
 
@@ -16,10 +16,10 @@ public:
         ans.push_back(ap[ind]);
         k -= ind * ini;
 
-        for (int i = n - 1; i >= 1; i--) {
+        for (int i = n; i >= 1; i--) {
             ini>>=1;
-            ind = (k-1) / ini;
-            k -= ind * ini;
+            ind = (k-1)/ini;
+            k -= ind*ini;
             ans.push_back(m.at(ans.back())[ind]);
         }
         return ans;
