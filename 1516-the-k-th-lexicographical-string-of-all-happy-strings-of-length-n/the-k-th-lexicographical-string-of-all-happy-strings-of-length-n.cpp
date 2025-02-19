56ac8bc95bcebc78a8d731b78243efe46ea74a02
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-19 12:04:49 +1100

    Time: 0 ms (100.00%) | Memory: 8 MB (87.05%) - LeetSync

---
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -0,0 +1,26 @@
+char ap[3] = {'a', 'b', 'c'};
+const unordered_map<char, vector<char>> m = {
+    {'a', {'b', 'c'}}, {'b', {'a', 'c'}}, {'c', {'a', 'b'}}};
+    
+class Solution {
+public:
+    string getHappyString(int n, int k) {
+        int ini = 1 << (n - 1);
+        if (k > 3 * ini)
+            return ""; // Fix the boundary check
+
+        string ans;
+        int ind = (k - 1) / ini;
+        ans.push_back(ap[ind]);
+        k -= ind * ini; // Adjust k for the remaining sequence
+
+        for (int i = n - 1; i >= 1; i--) {
+            ini /= 2;
+            ind = (k - 1) / ini;         // Ensure ind remains valid
+            k -= ind * ini;              // Adjust k
+            char last_char = ans.back(); // Fix shadowing issue
+            ans.push_back(m.at(last_char)[ind]);
+        }
+        return ans;
+    }
+};
