2f8510c6298addc78a0d54a6ebd70edf2f7e530a
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-21 12:24:40 +1100

    Time: 3 ms (94.21%) | Memory: 26 MB (87.66%) - LeetSync

--- 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
+++ 1387-find-elements-in-a-contaminated-binary-tree/find-elements-in-a-contaminated-binary-tree.cpp
@@ -12,44 +12,53 @@
 class FindElements {
 TreeNode* root;
 public:
-    FindElements(TreeNode* root) : root(root) { // Store root pointer
-        reconstruct(root, 0);  // Use helper function for cleaner code
-    }
-
-    void reconstruct(TreeNode* node, int val) {
-        if (!node) return;
-        node->val = val;
-        reconstruct(node->left, (val << 1) + 1);
-        reconstruct(node->right, (val << 1) + 2);
+    FindElements(TreeNode* root) : root(root) {
+        if(root->val == -1){
+            root->val = 0;
+            if(root->left!=nullptr){
+                root->left->val = 1;
+                FindElements(root->left);
+            }
+            if(root->right!=nullptr){
+                root->right->val = 2;
+                FindElements(root->right);
+            }
+        }else{
+            if(root->left!=nullptr){
+                root->left->val = (root->val<<1)+1;
+                FindElements(root->left);
+            }
+            if(root->right!=nullptr){
+                root->right->val = (root->val<<1)+2;
+                FindElements(root->right);
+            }
+        }
     }
     
     bool find(int target) {
-        TreeNode* obj = root; // Start from root every time
-        vector<bool> op; // 0 left, 1 right
-
-        while (target != 0) {
-            if (target % 2) { // Odd -> left
-                (--target)>>=1;
-                op.push_back(false);
-            } else { // Even -> right
-                (target-=2)>>=1;
-                op.push_back(true);
+        TreeNode* obj = root;
+        vector<bool> op; // 0 left-odd, 1 right-even
+        while(target!=0){
+            if(target%2){//odd
+            (--target)>>=1;
+            op.push_back(false);
+            }else{
+               target = (target-2)>>1;
+            op.push_back(true); 
             }
         }
-
-        for (int i = (int)op.size() - 1; i >= 0; i--) {
-            if (op[i]) { // Go right
-                if (!obj->right) return false;
+        for(int i = (int)op.size()-1;i>=0;i--){
+            if(op[i]){
+                if(obj->right == nullptr) return false;
                 obj = obj->right;
-            } else { // Go left
-                if (!obj->left) return false;
+            }else{
+                if(obj->left == nullptr) return false;
                 obj = obj->left;
-            }
+            }// right
         }
         return true;
     }
 };
-
 
 /**
  * Your FindElements object will be instantiated and called as such:
