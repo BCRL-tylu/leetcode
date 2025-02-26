d97d7f3f433f9e61134937e159957bdca01ea8ec
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-26 11:52:27 +1100

    Time: 0 ms (100.00%) | Memory: 45.2 MB (20.65%) - LeetSync

--- 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
+++ 1849-maximum-absolute-sum-of-any-subarray/maximum-absolute-sum-of-any-subarray.cpp
@@ -2,10 +2,8 @@ class Solution {
 public:
     int maxAbsoluteSum(vector<int>& nums) {
         int n = nums.size(), prefix = 0, mi = 0, ma = 0;
-        vector<int> ps;
         for(int i =0;i<n;i++){
             prefix+=nums[i];
-            ps.push_back(prefix);
             mi = min(mi,prefix);
             ma = max(ma,prefix);
         }
