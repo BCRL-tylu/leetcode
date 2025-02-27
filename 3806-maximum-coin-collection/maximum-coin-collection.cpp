534a8a99ac1ac671795cb0dd39479ddd73bed482
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-02-27 16:43:15 +1100

    Time: 15 ms (85.71%) | Memory: 145.4 MB (90.48%) - LeetSync

--- 3806-maximum-coin-collection/maximum-coin-collection.cpp
+++ 3806-maximum-coin-collection/maximum-coin-collection.cpp
@@ -8,21 +8,20 @@ public:
         // dp0 - stay on line 1
         // dp1 - Switch line once
         // dp2 - switch line twice
-        vector<long long> dp0(n, NEG_INF), dp1(n, NEG_INF), dp2(n, NEG_INF);
-        
+        long long dp0 = NEG_INF,dp1 = NEG_INF, dp2 = NEG_INF;
         // Base case: at mile 0.
-        dp0[0] = lane1[0]; 
-        dp1[0] = lane2[0];
-        long long ans = max(dp0[0], dp1[0]);
+        dp0 = lane1[0]; 
+        dp1 = lane2[0];
+        long long ans = max(dp0, dp1);
         for (int i = 1; i < n; i++) {
-            // Stay in lane 1
-            dp0[i] = max((long long)lane1[i], dp0[i-1] + lane1[i]);
-             // Switch from lane 1 to lane 2 or stay in lane 2
-            dp1[i] = max({ (long long)lane2[i], dp1[i-1] + lane2[i], dp0[i-1] + lane2[i] });
             // Switch from lane 2 back to lane 1 or stay in lane 1
-            dp2[i] = max(dp2[i-1] + lane1[i], dp1[i-1] + lane1[i]);
+            dp2 = max(dp2 + lane1[i], dp1 + lane1[i]);
+            // Switch from lane 1 to lane 2 or stay in lane 2
+            dp1 = max({ (long long)lane2[i], dp1 + lane2[i], dp0 + lane2[i] });
+            // Stay in lane 1
+            dp0 = max((long long)lane1[i], dp0 + lane1[i]);
 
-            ans = max({ans, dp0[i], dp1[i], dp2[i] });
+            ans = max({ans, dp0, dp1, dp2});
         }
         return ans;
     }
