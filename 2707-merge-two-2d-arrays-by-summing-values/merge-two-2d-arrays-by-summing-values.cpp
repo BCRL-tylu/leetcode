ccd5b9c1e36663fea8c5cf1a47fdde6346378743
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-02 12:09:58 +1100

    Time: 0 ms (100.00%) | Memory: 14.6 MB (82.78%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        unordered_map<int,int> ans;
-        vector<vector<int>> rt;
-        for(int i =0;i<(int)nums1.size();i++){
-            ans[nums1[i][0]] = nums1[i][1];
-        }
-        
-        for(int i =0;i<(int)nums2.size();i++){
-            ans[nums2[i][0]] += nums2[i][1];
+        vector<vector<int>>ans;
+        int temp1=0, temp2=0;
+        int n1 = nums1.size(), n2 = nums2.size();
+        while(temp1<n1 && temp2<n2){
+            if(nums1[temp1][0]==nums2[temp2][0]){
+                ans.push_back({nums1[temp1][0],nums1[temp1][1]+nums2[temp2][1]});
+                temp1++;temp2++;
+            }
+            else if(nums1[temp1][0]<nums2[temp2][0]){
+                ans.push_back({nums1[temp1][0],nums1[temp1][1]});
+                temp1++;               
+            }
+            else{
+                ans.push_back({nums2[temp2][0],nums2[temp2][1]});
+                temp2++;                
+            }
         }
 
-        for(auto p:ans){
-            rt.push_back({p.first,p.second});
+        while(temp1<n1){
+            ans.push_back({nums1[temp1][0],nums1[temp1][1]});
+            temp1++; 
         }
-        sort(rt.begin(),rt.end());
-        return rt;
+
+        while(temp2<n2){
+            ans.push_back({nums2[temp2][0],nums2[temp2][1]});
+            temp2++; 
+        }
+        return ans; 
     }
 };
