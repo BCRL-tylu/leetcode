144e58f6be8fb8f58834fa06c6c7ed191fccb71f
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-04 17:14:44 +1100

    Time: 122 ms (100.00%) | Memory: 206.5 MB (100.00%) - LeetSync

--- 3818-maximize-profit-from-task-assignment/maximize-profit-from-task-assignment.cpp
+++ 3818-maximize-profit-from-task-assignment/maximize-profit-from-task-assignment.cpp
@@ -1,41 +1,25 @@
-using pgq = priority_queue<int, vector<int>, greater<int>>;
-
-class Solution {
-public:
-    long long maxProfit(vector<int>& workers, vector<vector<int>>& tasks) {
-        unordered_map<int, int> cw;
-        unordered_map<int, pgq> ts;
-
-        for (int k : workers) {
-            cw[k]++;
-        }
-
-        long long ans = 0;
-        int ma = 0;
-
-        for (const auto& task : tasks) {
-            int skill = task[0], profit = task[1];
-
-            auto it = cw.find(skill);
-            if (it == cw.end()) {
-                ma = max(ma, profit);
-                continue;
-            }
-
-            auto& pq = ts[skill]; 
-
-            if ((int)pq.size() < it->second) {
-                pq.push(profit);
-                ans += profit;
-            } else if (profit > pq.top()) {
-                ma = max(ma, pq.top()); 
-                ans += profit - pq.top();
-                pq.pop();
-                pq.push(profit);
-            } else {
-                ma = max(ma, profit);
-            }
-        }
-        return ans + ma;
+class Solution 
+{
+	public:
+    long long maxProfit(vector<int>& workers, vector<vector<int>>& tasks) 
+	{
+		long long result=0,maximum=0,m=workers.size(),n=tasks.size();
+		unordered_map<int,int> total;
+		sort(tasks.begin(),tasks.end(),[](vector<int>& a,vector<int>& b){
+			if(a[0]!=b[0]) return a[0]<b[0];
+			return a[1]>b[1];
+		});
+		for(int i=0;i<m;i++) total[workers[i]]++;
+		for(int i=0;i<n;i++)
+		{
+			if(total.count(tasks[i][0]))
+			{
+				result+=tasks[i][1];
+				total[tasks[i][0]]--;
+				if(total[tasks[i][0]]==0) total.erase(tasks[i][0]);
+			}
+			else maximum=max(maximum,1ll*tasks[i][1]);
+		}
+		return result+maximum;
     }
 };
