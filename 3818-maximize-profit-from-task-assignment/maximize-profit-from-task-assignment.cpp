0464afd15d29811d313e88eaef4a8be068bb5f56
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-04 17:14:35 +1100

    Time: 92 ms (100.00%) | Memory: 216.8 MB (100.00%) - LeetSync

--- 3818-maximize-profit-from-task-assignment/maximize-profit-from-task-assignment.cpp
+++ 3818-maximize-profit-from-task-assignment/maximize-profit-from-task-assignment.cpp
@@ -1,28 +1,41 @@
+using pgq = priority_queue<int, vector<int>, greater<int>>;
+
 class Solution {
 public:
-    long long maxProfit(vector<int>& W, vector<vector<int>>& T) {
-        unordered_map<int,int> c;
-        for (int a : W) c[a]++;
-        unordered_map<int,vector<int>> b;
-        for (auto &t : T) b[t[0]].push_back(t[1]);
-        long long s = 0;
-        int x = 0;
-        for (auto &p : b) {
-            auto &v = p.second;
-            if (c.find(p.first) == c.end())
-                x = max(x, *max_element(v.begin(), v.end()));
-            else {
-                int av = c[p.first], n = v.size();
-                if (n <= av)
-                    s += accumulate(v.begin(), v.end(), 0LL);
-                else {
-                    nth_element(v.begin(), v.end() - av, v.end());
-                    s += accumulate(v.begin() + n - av, v.end(), 0LL);
-                    int m = *max_element(v.begin(), v.begin() + n - av);
-                    x = max(x, m);
-                }
+    long long maxProfit(vector<int>& workers, vector<vector<int>>& tasks) {
+        unordered_map<int, int> cw;
+        unordered_map<int, pgq> ts;
+
+        for (int k : workers) {
+            cw[k]++;
+        }
+
+        long long ans = 0;
+        int ma = 0;
+
+        for (const auto& task : tasks) {
+            int skill = task[0], profit = task[1];
+
+            auto it = cw.find(skill);
+            if (it == cw.end()) {
+                ma = max(ma, profit);
+                continue;
+            }
+
+            auto& pq = ts[skill]; 
+
+            if ((int)pq.size() < it->second) {
+                pq.push(profit);
+                ans += profit;
+            } else if (profit > pq.top()) {
+                ma = max(ma, pq.top()); 
+                ans += profit - pq.top();
+                pq.pop();
+                pq.push(profit);
+            } else {
+                ma = max(ma, profit);
             }
         }
-        return s + x;
+        return ans + ma;
     }
 };
