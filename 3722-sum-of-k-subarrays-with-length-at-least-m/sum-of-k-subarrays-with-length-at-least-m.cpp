a7157b07fa76a31011c315b1f18e437f13c83c0f
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-06 15:31:43 +1100

    Time: 177 ms (87.70%) | Memory: 233.2 MB (19.26%) - LeetSync

--- 3722-sum-of-k-subarrays-with-length-at-least-m/sum-of-k-subarrays-with-length-at-least-m.cpp
+++ 3722-sum-of-k-subarrays-with-length-at-least-m/sum-of-k-subarrays-with-length-at-least-m.cpp
@@ -3,30 +3,30 @@ public:
     int maxSum(vector<int>& nums, int k, int m) {
         int n = nums.size();
 
-        vector<int> ps(n+1, 0);
+        vector<int> ps(n + 1, 0);
         for (int i = 0; i < n; i++) {
-            ps[i+1] = ps[i] + nums[i];
+            ps[i + 1] = ps[i] + nums[i];
         }
 
-        vector<vector<int>> dp(k+1, vector<int>(n+1, INT_MIN)),local(k+1, vector<int>(n+1, INT_MIN));
-        
+        const int neg_inf = -1e8 - 1;
+        vector<vector<int>> dp(k + 1, vector<int>(n + 1, neg_inf)),
+            local(k + 1, vector<int>(n + 1, neg_inf));
+
         for (int j = 0; j <= n; j++) {
             dp[0][j] = 0;
         }
 
         for (int i = 1; i <= k; i++) {
-            for (int j = i * m; j <= n; j++) {
-                if(j == i * m) {
-                    local[i][j] = dp[i-1][j-m] + (ps[j] - ps[j-m]);
-                } else {
-                    local[i][j] = local[i][j-1] + nums[j-1];
-                    local[i][j] = max(local[i][j], dp[i-1][j-m] + (ps[j] - ps[j-m]));
-                }
-
-                dp[i][j] = max(dp[i][j-1], local[i][j]);
+            int u = i * m;
+            local[i][u] = dp[i - 1][u - m] + (ps[u] - ps[u - m]);
+            dp[i][u] = local[i][u]; // Update dp for the first valid index
+            for (int j = u + 1; j <= n; j++) {
+                local[i][j] = max(local[i][j - 1] + nums[j - 1],
+                                  dp[i - 1][j - m] + (ps[j] - ps[j - m]));
+                dp[i][j] = max(dp[i][j - 1], local[i][j]);
             }
         }
-        
+
         return dp[k][n];
     }
 };
