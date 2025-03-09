49941f2e52d46b6daeb318417c8de766b4e6df92
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-09 13:13:56 +1100

    Time: 76 ms (37.24%) | Memory: 102.6 MB (76.78%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& c, int k) {
-        int n = c.size();
-        int l = 0, r = 0, ans = 0;
-        // mod_r tracks r % n so we don't recompute it each time
-        int mod_r = 0;
-        
-        while(l < n) {
-            // Try to extend the segment until its length is k or alternation fails.
-            while(r - l + 1 < k) {
-                // Instead of (r+1) % n, we use a conditional:
-                int mod_next = (mod_r == n - 1) ? 0 : mod_r + 1;
-                if(c[mod_r] != c[mod_next]) {
-                    r++;
-                    mod_r = mod_next;  // update mod_r to the new r modulo n
-                } else {
-                    break;
-                }
+        int n = c.size(),l = 0,r=0,ans=0,tr;
+
+        while(l<n){
+            tr = r+1;
+            while(tr-l<k && c[r%n]!=c[(tr)%n]){
+                tr = ++r+1;
             }
-            if(r - l + 1 < k) {
-                // If the current segment isn't long enough, skip to the next candidate segment.
+            if(tr-l<k){
                 r++;
-                // update mod_r using a conditional to avoid modulo call if possible
-                mod_r = (r >= n) ? r - n : r;
-                l = r;
-            } else {
-                // If a valid group of length k is found, count it and move the left pointer.
+                l=r;
+            }else{
                 l++;
                 ans++;
             }
