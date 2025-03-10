8cac0ad8bd6fa0b049149d6ec552e7cb7a90d052
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-10 16:10:03 +1100

    Time: 172 ms (50.00%) | Memory: 181.2 MB (25.00%) - LeetSync

---
+++ 3759-choose-k-elements-with-maximum-sum/choose-k-elements-with-maximum-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<long long> findMaxSum(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+        vector<long long> ans(n, 0);
+        vector<pair<int, int>> n1(n);
+        for (int i = 0; i < n; i++) {
+            n1[i] = {nums1[i], i};
+        }
+        sort(n1.begin(), n1.end());
+        
+        // Use a multiset to allow duplicate values.
+        multiset<long long> heap;
+        long long k_largest = 0;
+        
+        for (int i = 0; i < n; i++) { // Process from smaller to larger nums1 values.
+            long long pre_k_larg = k_largest;
+            while (i + 1 < n && n1[i].first == n1[i + 1].first) {
+                int index = n1[i].second;
+                heap.insert(nums2[index]);
+                k_largest += nums2[index];
+                // If the heap size exceeds k, remove the smallest element.
+                if (heap.size() > k) {
+                    k_largest -= *heap.begin();
+                    heap.erase(heap.begin());
+                }
+                ans[index] = pre_k_larg;
+                i++;
+            }
+            if (i != n) { // Process the last element of the current group.
+                int index = n1[i].second;
+                heap.insert(nums2[index]);
+                k_largest += nums2[index];
+                if (heap.size() > k) {
+                    k_largest -= *heap.begin();
+                    heap.erase(heap.begin());
+                }
+                ans[index] = pre_k_larg;
+            }
+        }
+        return ans;
+    }
+};
