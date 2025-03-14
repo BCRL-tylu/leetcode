d7e7800c408714a037cfe5a3eff567ff07b691a4
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-14 13:31:11 +1100

    Time: 22 ms (77.72%) | Memory: 88.3 MB (41.19%) - LeetSync

---
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumCandies(vector<int>& c, long long k) {
+        int l = 1, r = *max_element(c.begin(), c.end()), n = c.size(), mid;
+        
+        while (l <= r) { 
+            mid = (l + r) >> 1; 
+            long long kids = 0; 
+            for (int i = 0; i < n; i++) kids += c[i] / mid;
+            
+            if (kids < k) { 
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return r; 
+    }
+};
