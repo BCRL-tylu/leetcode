62fb338f6bbe2f977c0e6908296a0b423362bb57
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-15 15:59:30 +1100

    Time: 28 ms (40.75%) | Memory: 60.8 MB (67.69%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int minCapability(vector<int>& nums, int k) {
-        int l = 0;
+        int l = *min_element(nums.begin(), nums.end());
         int r = *max_element(nums.begin(), nums.end());
 
         while (l < r) {
             int mid = (l + r) / 2;
             int count = 0, prev_robbed = -2;
-            
+            bool  valid =false;
             for (int i = 0; i < nums.size(); i++) {
                 if (nums[i] <= mid && i > prev_robbed + 1) {
                     count++;
                     prev_robbed = i;
                 }
+                if(count >= k){
+                    valid=true;
+                    break;
+                }
             }
 
-            if (count >= k) {
+            if (valid) {
                 r = mid;  // Try smaller capability
             } else {
                 l = mid + 1;  // Increase capability
