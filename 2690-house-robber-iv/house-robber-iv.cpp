8241a74fde41c56c98a0e69bf4262338a601ee12
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-15 15:55:32 +1100

    Time: 31 ms (30.84%) | Memory: 60.9 MB (40.42%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     int minCapability(vector<int>& nums, int k) {
-        int n = nums.size(), count = 0;
-        vector<pair<int, int>> m(n);
-        for (int i = 0; i < n; i++)
-            m[i] = {nums[i], i};
-        sort(m.begin(), m.end());
-        if(k == 1) return m[0].first;
-        int l = m[0].first, r = m[n - 1].first, mid;
+        int l = *min_element(nums.begin(), nums.end());
+        int r = *max_element(nums.begin(), nums.end());
+
         while (l < r) {
-            mid = (r + l - 1) >> 1;
-            int vc = 0;
-            for (int i = 0; m[i].first <= mid; i++) {
-                vc++;
+            int mid = (l + r) / 2;
+            int count = 0, prev_robbed = -2;
+            
+            for (int i = 0; i < nums.size(); i++) {
+                if (nums[i] <= mid && i > prev_robbed + 1) {
+                    count++;
+                    prev_robbed = i;
+                }
             }
-            if (vc < k) { // not valid, not enought number smaller than mid to meet the capacity
-                l = mid+1;
+
+            if (count >= k) {
+                r = mid;  // Try smaller capability
             } else {
-                vector<int> length(n, 0);
-                for (int i = 0; m[i].first <= mid; i++) 
-                    length[m[i].second]=1;
-                
-                bool valid =false;
-                length[1] = max(length[1],length[0]);
-                for (int i = 2; i < n; i++) {
-                    length[i] = max(length[i-2]+length[i],length[i-1]);
-                    if(length[i]>=k){
-                        valid = true;
-                        break;
-                    }
-                }
-                if (valid) {
-                    r = mid;
-                } else {
-                    l = mid + 1;
-                }
+                l = mid + 1;  // Increase capability
             }
         }
         return l;
