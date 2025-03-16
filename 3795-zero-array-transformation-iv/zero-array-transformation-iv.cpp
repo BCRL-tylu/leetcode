aa087ab30a3f1b5a56fee2901cbe58ef649e27a6
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-16 16:10:35 +1100

    Time: 11 ms (50.00%) | Memory: 47.1 MB (91.67%) - LeetSync

--- 3795-zero-array-transformation-iv/zero-array-transformation-iv.cpp
+++ 3795-zero-array-transformation-iv/zero-array-transformation-iv.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
-    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        int m = queries.size(), n = nums.size();
+    int minZeroArray(vector<int>& a, vector<vector<int>>& q) {
+        int m = q.size(), n = a.size();
         int L = 0, R = m, ans = -1;
-        
-        while (L <= R) {
+        while(L <= R){
             int mid = L + (R - L) / 2;
-            if (check(mid, nums, queries)) {
+            if(check(mid, a, q)){
                 ans = mid;
-                R = mid - 1;  // Try to find a smaller k
+                R = mid - 1;
             } else {
-                L = mid + 1;  // We need more queries
+                L = mid + 1;
             }
         }
         return ans;
     }
-
 private:
-    bool check(int k, vector<int>& nums, vector<vector<int>>& queries) {
-        int n = nums.size();
-        // For each index, we try to see if we can select a subset of queries (among the first k)
-        // covering that index to exactly sum up to nums[i].
-        for (int i = 0; i < n; i++) {
-            int target = nums[i];
-            vector<bool> dp(target + 1, false);
-            dp[0] = true;
-            // Process the first k queries
-            for (int j = 0; j < k; j++) {
-                // Only consider query j if it affects index i
-                if (queries[j][0] <= i && i <= queries[j][1]) {
-                    int val = queries[j][2];
-                    // Standard subset-sum update (0/1 knapsack) updating backwards
-                    for (int t = target; t >= val; t--) {
-                        if (dp[t - val]) {
-                            dp[t] = true;
-                        }
-                    }
-                }
+    bool check(int k, vector<int>& a, vector<vector<int>>& q){
+        int n = a.size();
+        for(int i = 0; i < n; i++){
+            bitset<1001> dp;
+            dp.reset();
+            dp[0] = 1;
+            for(int j = 0; j < k; j++){
+                if(q[j][0] <= i && i <= q[j][1])
+                    dp |= (dp << q[j][2]);
             }
-            // If we cannot form the sum equal to nums[i], then index i cannot be zeroed.
-            if (!dp[target]) return false;
+            if(!dp.test(a[i])) return false;
         }
         return true;
     }
