04821405b4c1041447fc85b1ac7afabb6a237219
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-18 16:19:21 +1100

    Time: 11 ms (100.00%) | Memory: 42.7 MB (100.00%) - LeetSync

--- 3836-phone-number-prefix/phone-number-prefix.cpp
+++ 3836-phone-number-prefix/phone-number-prefix.cpp
@@ -1,13 +1,38 @@
+struct TrieNode {
+    TrieNode* c[10] = {nullptr};
+    bool isEnd = false;
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    Trie() {
+        root = new TrieNode();
+    }
+};
 
 class Solution {
 public:
     bool phonePrefix(vector<string>& numbers) {
-        sort(numbers.begin(), numbers.end());
-        for (size_t i = 0; i + 1 < numbers.size(); i++) {
-            if (numbers[i+1].size() >= numbers[i].size() &&
-                numbers[i+1].compare(0, numbers[i].size(), numbers[i]) == 0)
-                return false;
+        Trie trie;
+        for (const string& num : numbers) {
+            TrieNode* node = trie.root;
+            for (char d : num) {
+                int index = d - '0';
+                if (node->isEnd) return false;
+                if (!node->c[index]) node->c[index] = new TrieNode();
+                node = node->c[index];
+            }
+            if (node->isEnd) return false;
+            if (has(node)) return false;
+            node->isEnd = true;
         }
         return true;
+    }
+
+private:
+    bool has(TrieNode* node) {
+        for (int i = 0; i < 10; i++) if (node->c[i]) return true;
+        return false;
     }
 };
