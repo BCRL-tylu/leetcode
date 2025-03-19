3e63587ba439f35782a832f80e172ae886b64625
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-19 17:31:59 +1100

    Time: 8 ms (77.78%) | Memory: 372.7 MB (46.03%) - LeetSync

--- 3758-minimum-unlocked-indices-to-sort-nums/minimum-unlocked-indices-to-sort-nums.cpp
+++ 3758-minimum-unlocked-indices-to-sort-nums/minimum-unlocked-indices-to-sort-nums.cpp
@@ -2,23 +2,19 @@ class Solution {
 public:
     int minUnlockedIndices(vector<int>& nums, vector<int>& locked) {
         int minUnlocked = 0;
-        for (int i = 0, numLocksFromMax = 0, maxNum = -1; i < nums.size();
-             ++i) {
-
-            if (maxNum < nums[i]) {
+        for(int i=0,numLocksFromMax=0, maxNum=-1; i<nums.size();++i) {
+            if(maxNum < nums[i]){
                 maxNum = nums[i];
                 numLocksFromMax = 0;
-            }
-
-            else if (maxNum > nums[i]) {
+            }else if (maxNum > nums[i]) {
                 if ((maxNum-nums[i])&1) {
-                    minUnlocked += numLocksFromMax;
-                    numLocksFromMax = 0;
+                    minUnlocked+=numLocksFromMax;
+                    numLocksFromMax=0;
                 } else {
                     return -1;
                 }
             }
-            numLocksFromMax += locked[i];
+            numLocksFromMax+=locked[i];
         }
         return minUnlocked;
     }
