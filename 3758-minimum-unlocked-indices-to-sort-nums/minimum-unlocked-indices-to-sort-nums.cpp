c108eba66dc40b374a037c3293a8d81752ad16d0
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-19 17:14:33 +1100

    Time: 11 ms (61.90%) | Memory: 372.7 MB (46.03%) - LeetSync

--- 3758-minimum-unlocked-indices-to-sort-nums/minimum-unlocked-indices-to-sort-nums.cpp
+++ 3758-minimum-unlocked-indices-to-sort-nums/minimum-unlocked-indices-to-sort-nums.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
     int minUnlockedIndices(vector<int>& nums, vector<int>& locked) {
-        int ones = 0; 
-        int twos = 0; 
-        int threes = 0; 
-        int pos = 0;
-        int ret = 0;
-        vector<int> sums(nums.size());
-        sums[0] = locked[0]; 
-        for(int i = 1;i<locked.size();i++) {
-            sums[i] = sums[i-1] + locked[i];
-        }
-        for(int i = 0;i<nums.size();i++) {
-            if(nums[i] == 1) {
-                if(threes > 0) {
-                    return -1;
-                }
-                if(twos > 0) {
-                    int l = max(pos, ones);
-                    ret += sums[i-1] - sums[l] + locked[l];
-                    pos = i;
-                }
-                ones++;
-            } else if(nums[i] == 2) {
-                if(threes > 0) {
-                    int l = max(ones + twos, pos); 
-                    ret += sums[i-1] - sums[l] + locked[l];
-                    pos = i;
-                }
-                twos++; 
-            } else {
-                threes++; 
+        int minUnlocked = 0;
+        for (int i = 0, numLocksFromMax = 0, maxNum = INT_MIN; i < nums.size(); ++i) {
+            if (maxNum < nums[i]) {
+                maxNum          = nums[i];
+                numLocksFromMax = 0;
             }
+            else if (maxNum > nums[i]) {
+                if (maxNum > (nums[i]+1))
+                    return -1;
+
+                minUnlocked += exchange(numLocksFromMax, 0);
+            }
+            numLocksFromMax += locked[i];
         }
-        return ret; 
+        return minUnlocked;
     }
 };
