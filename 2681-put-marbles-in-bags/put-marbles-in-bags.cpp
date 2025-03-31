b6dfd9d8e762f64063707b7fbaf3d0ad66f331db
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-03-31 15:43:02 +1100

    Time: 34 ms (73.84%) | Memory: 73 MB (26.58%) - LeetSync

---
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size();
+        if (k == 1) return 0;
+        long long base = (long long)weights.front() + weights.back();
+        vector<long long> contributions;
+        for (int i = 0; i < n - 1; i++)  contributions.push_back(weights[i] + weights[i+1]);
+
+        
+        // Sort contributions so that we can choose the smallest for the minimal score
+        // and the largest for the maximum score.
+        sort(contributions.begin(), contributions.end());
+        
+        long long minScore = base, maxScore = base;
+        // For minimal score, add the (k-1) smallest contributions.
+        for (int i = 0; i < k - 1; i++) {
+            minScore += contributions[i];
+        }
+        // For maximum score, add the (k-1) largest contributions.
+        for (int i = 0; i < k - 1; i++) {
+            maxScore += contributions[contributions.size() - 1 - i];
+        }
+        return maxScore - minScore;
+    }
+};
