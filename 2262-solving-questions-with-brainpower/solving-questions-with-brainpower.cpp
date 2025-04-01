4443244edadfb4f0653630f0c6d1c8b37d31cd3f
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-04-01 13:46:19 +1100

    Time: 3 ms (79.77%) | Memory: 119.8 MB (74.65%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
-    long long mostPoints(vector<vector<int>>& questions) {
-        long long n = questions.size(),ans =0;
+    long long mostPoints(vector<vector<int>>& q) {
+        long long n = q.size(),ans =0;
         vector<long long> dp(n+1,0);
         for(int i =1;i<=n;i++){
             dp[i]=max(dp[i],dp[i-1]);
-            int ind = i+questions[i-1][1]+1;
+            int ind = i+q[i-1][1]+1;
             if(ind>n){
-                ans = max(ans,dp[i]+questions[i-1][0]);
+                ans = max(ans,dp[i]+q[i-1][0]);
                 continue;
             }
-            dp[ind] = max(dp[ind],dp[i]+questions[i-1][0]);
+            dp[ind] = max(dp[ind],dp[i]+q[i-1][0]);
         }
-        return max(ans,dp[n-1]);
+        return max(ans,dp[n]);
     }
 };
