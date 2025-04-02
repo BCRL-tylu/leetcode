3e554a2d8f50e5cc7978878814477135b8f53221
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-04-02 17:10:19 +1100

    Time: 0 ms (100.00%) | Memory: 22.8 MB (5.12%) - LeetSync

---
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+public:
+    long long maximumTripletValue(vector<int>& nums) {
+        long long ans=INT_MIN, n=nums.size();
+        long long i =0, k=0;
+        vector<long long> ma(n,0);
+        ma[n-1] = nums[n-1];
+        for(long long k=n-2;k>=0;k--){
+            ma[k] = max((long long)nums[k],ma[k+1]);
+        }
+        for(long long j=1;j<n-1;j++){
+            i = max(i,(long long)nums[j-1]);
+            ans = max(ans, (i-nums[j])*ma[j+1]);
+        }
+        return ans>0?ans:0;
+    }
+};
