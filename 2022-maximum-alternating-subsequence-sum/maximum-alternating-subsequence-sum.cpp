eee3479b09ae051633cd3078057563ea5862f57b
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-04-08 19:16:25 +1000

    Time: 249 ms (47.09%) | Memory: 157.2 MB (29.54%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -5,8 +5,9 @@ public:
         vector<vector<long long>> dp(n,vector<long long>(2));
         dp[0][0] = nums[0];
         for(int i =0;i<n-1;i++){
-            dp[i+1][0] = max(dp[i][1]+nums[i+1],dp[i][0]);
-            dp[i+1][1] = max(dp[i][0]-nums[i+1],dp[i][1]);
+            int k = i+1;
+            dp[k][0] = max(dp[i][1]+nums[k],dp[i][0]);
+            dp[k][1] = max(dp[i][0]-nums[k],dp[i][1]);
         }
         return dp[n-1][0];
     }
