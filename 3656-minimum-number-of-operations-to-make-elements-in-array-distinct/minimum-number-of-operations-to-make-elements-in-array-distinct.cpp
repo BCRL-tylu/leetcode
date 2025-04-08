96f100d139affb15583256f9a7e5b798dcd5964c
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-04-08 12:11:33 +1000

    Time: 7 ms (32.42%) | Memory: 29.1 MB (24.57%) - LeetSync

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -8,13 +8,11 @@ public:
     int minimumOperations(vector<int>& nums) {
         unordered_map<int,int> freq,multi;
         for(auto nu:nums) if(++freq[nu]>1) multi[nu]=1;
-        int i =0,op=0;
-        while(i<nums.size()){
-            int temp = i;
-            if(multi.size()==0){
-                break;
-            }
-            while(i<temp+3 && i<nums.size()){
+        int i =0,op=0,n=nums.size();
+        while(i<n){
+            int temp = i+3;
+            if(multi.size()==0) break;
+            while(i<temp && i<n){
                 if(--freq[nums[i]]==1) multi.erase(nums[i]);
                 i++;
             }
