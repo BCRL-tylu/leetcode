74047eb3e8d7bca2212a432c8fdbfa7321b0bd80
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-04-08 19:39:52 +1000

    Time: 122 ms (96.54%) | Memory: 54.7 MB (94.81%) - LeetSync

--- 3777-maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k/maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k.cpp
+++ 3777-maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k/maximum-product-of-subsequences-with-an-alternating-sum-equal-to-k.cpp
@@ -1,41 +1,37 @@
-const int MIN = -5000;
-
 class Solution {
 public:
-    unordered_map<int, unordered_map<int, unordered_map<int, unordered_map<int, int>>>> dp;
-    int recursion(int pos, int currSum, int product, int isOdd, int k, int n, vector<int> &nums, int limit) {
-        if(pos == n) {
-            return (currSum == k && isOdd != 0 && product <= limit ? product : MIN);
+    int maxProduct(vector<int>& nums, int k, int limit) {
+        if (abs(k) > 900) return -1;
+        vector<vector<bitset<1801>>> dp(2, vector<bitset<1801>>(limit + 1));
+        vector<vector<bitset<1801>>> any(2, vector<bitset<1801>>(2));
+        dp[0][1][900] = any[0][0][900] = 1;
+        bitset<1801> tmp;
+        for (int i : nums) {
+            for (int j = limit; j >= 0; j--) {
+                if (j * i > limit) continue;
+                tmp = dp[0][j];
+                dp[0][j * i] |= dp[1][j] >> i;
+                dp[1][j * i] |= tmp << i;
+            }
+            for (int j = 1; j >= 0; j--) {
+                tmp = any[0][j];
+                any[0][j || i == 0] |= any[1][j] >> i;
+                any[1][j || i == 0] |= tmp << i;
+            }
         }
-        
-        if(dp.count(pos) && dp[pos].count(currSum) && dp[pos][currSum].count(product) && dp[pos][currSum][product].count(isOdd)) {
-            return dp[pos][currSum][product][isOdd];
+        int res = -1;
+        for (int x = limit; x >= 1; x--) {
+            if (dp[0][x][900 + k] || dp[1][x][900 + k]) {
+                if (k == 0 && x == 1) {
+                    int ones = 0;
+                    for (int i : nums) if (i == 1) ones++;
+                    if (ones >= 2) return 1;
+                } else {
+                    return x;
+                }
+            }
         }
-            
-        int ans = recursion(pos + 1, currSum, product, isOdd, k, n, nums, limit);
-        if(isOdd == 0) {
-            ans = max(ans, recursion(pos + 1, currSum + nums[pos], nums[pos], 2, k, n, nums, limit));
-        }
-        if(isOdd == 1) {
-            ans = max(ans, recursion(pos + 1, currSum + nums[pos], min(product * nums[pos], limit + 1), 2, k, n, nums, limit));
-        }
-        if(isOdd == 2) {
-            ans = max(ans, recursion(pos + 1, currSum - nums[pos], min(product * nums[pos], limit + 1), 1, k, n, nums, limit));
-        }
-        
-        return dp[pos][currSum][product][isOdd] = ans;
-    }
-    
-    int maxProduct(vector<int> &nums, int k, int limit) {
-        int n = nums.size();
-        
-        int sum = 0;
-        for(auto &x: nums) sum += x;
-        
-        if(k > sum || k < -sum) return -1;
-            
-        dp.clear();
-        int ans = recursion(0, 0, 0, 0, k, n, nums, limit);
-        return (ans == MIN) ? -1 : ans;
+        if (any[0][1][900 + k] || any[1][1][900 + k]) return 0;
+        return -1;
     }
 };
