2057829fe00315f9c71f5e986c808e5e9776cea5
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-04-08 18:55:40 +1000

    Time: 0 ms (100.00%) | Memory: 117.4 MB (100.00%) - LeetSync

--- 3855-make-a-positive-array/make-a-positive-array.cpp
+++ 3855-make-a-positive-array/make-a-positive-array.cpp
@@ -4,12 +4,14 @@ public:
         int ans = 0;
         long long sm = INT_MAX, mx = 1e18;
         long long a = nums[0], b = nums[1];
-
+        
         for (int i = 2; i < nums.size(); ++i) {
             long long c = nums[i];
             sm = c + min(sm, a + b);
             if (sm <= 0) {
                 c = mx;
+                // seperator indicates that the segement prior to this bit is eligble
+                // if not updated, means we can potentially have hazard of getting below 0
                 sm = mx;
                 ans++;
             }
