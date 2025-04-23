cf2a28d6c0b21c3062e0bca4c5061a8718c49e25
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-04-23 11:07:41 +1000

    Time: 3 ms (45.04%) | Memory: 23.3 MB (22.90%) - LeetSync

--- 302-smallest-rectangle-enclosing-black-pixels/smallest-rectangle-enclosing-black-pixels.cpp
+++ 302-smallest-rectangle-enclosing-black-pixels/smallest-rectangle-enclosing-black-pixels.cpp
@@ -3,34 +3,33 @@ public:
     int minArea(vector<vector<char>>& image, int x, int y) {
         int m = image.size(), n = image[0].size();
         vector<vector<bool>> seen(m, vector<bool>(n, false));
-        int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
-        
-        // Initialize bounding-box to include the starting black pixel
+        int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
         int minRow = x, maxRow = x;
         int minCol = y, maxCol = y;
-        
-        queue<pair<int,int>> q;
-        q.emplace(x,y);
+
+        queue<pair<int, int>> q;
+        q.emplace(x, y);
         seen[x][y] = true;
-        
+
         while (!q.empty()) {
-            auto [r,c] = q.front(); q.pop();
-            
-            // Expand bounding box
-            minRow = min(minRow, r);
-            maxRow = max(maxRow, r);
-            minCol = min(minCol, c);
-            maxCol = max(maxCol, c);
-            for (auto &d : dirs) {
+            auto [r, c] = q.front();
+            q.pop();
+            for (auto& d : dirs) {
                 int nr = r + d[0], nc = c + d[1];
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
-                if (seen[nr][nc] || image[nr][nc] != '1') continue; 
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n)
+                    continue;
+                if (seen[nr][nc] || image[nr][nc] != '1')
+                    continue;
                 seen[nr][nc] = true;
-                q.emplace(nr,nc);
+                q.emplace(nr, nc);
+                minRow = min(minRow, nr);
+                maxRow = max(maxRow, nr);
+                minCol = min(minCol, nc);
+                maxCol = max(maxCol, nc);
             }
         }
-        
-        // Width × Height
+
         return (maxRow - minRow + 1) * (maxCol - minCol + 1);
     }
 };
