de907fe2ce019aa8b4db5bd5d194784120b44d37
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-05-08 15:02:30 +1000

    Time: 217 ms (82.91%) | Memory: 97.2 MB (79.96%) - LeetSync

---
+++ 3628-find-minimum-time-to-reach-last-room-ii/find-minimum-time-to-reach-last-room-ii.cpp
@@ -0,0 +1,32 @@
+using ll =long long;
+using T3 = tuple<int,int,int,int>;
+
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+         int n = moveTime.size(), m = moveTime[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        priority_queue<T3,vector<T3>,greater<T3>> pq;
+
+        dist[0][0] = 0;
+        pq.emplace(0, 0, 0, 0);
+        int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        while (!pq.empty()) {
+            auto [t,i,j,o] = pq.top(); 
+            pq.pop();
+            if (t > dist[i][j]) continue;
+            if (i == n-1 && j == m-1) return t;
+            for (auto& d : dirs) {
+                int x = i + d[0], y = j + d[1];
+                if (x<0 || x>=n || y<0 || y>=m) continue;
+                ll nt = max(t, moveTime[x][y]) + (o==0?1:2);
+                if (nt < dist[x][y]) {
+                    dist[x][y] = nt;
+                    pq.emplace(nt, x, y, o==0?1:0);
+                }
+            }
+        }
+        return -1;
+    }
+};
