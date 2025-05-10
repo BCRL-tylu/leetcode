dace3bc6e1db2c4460d9ac9bd37873dd0471a6c4
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-05-10 19:11:14 +1000

    Time: 143 ms (17.63%) | Memory: 135.2 MB (93.56%) - LeetSync

---
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        long long l1=0,l2=0,s1=0,s2=0;
+        for(auto k:nums1) {
+            if(!k) l1++;
+            s1+=(long long)k;
+        }
+        for(auto k:nums2) {
+            if(!k) l2++;
+            s2+=(long long)k;
+        }
+        long long k1 = s1+l1, k2=s2+l2;
+        if(k1==k2) return k1; 
+        if(k1>k2){
+            if(l2!=0) return k1;
+            else return -1;
+        }
+        if(k1<k2){
+            if(l1!=0) return k2;
+            else return -1;
+        }
+        return -1;
+    }
+};
