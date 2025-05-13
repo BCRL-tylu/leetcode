1bf2e4bc0fe09a44da1f8fbd793d3e7620d65138
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-05-13 16:17:58 +1000

    Time: 81 ms (80.30%) | Memory: 18.9 MB (98.03%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int lengthAfterTransformations(string s, int t) {
-        int ans = s.size(), mod = 1e9 + 7;
+        int ans = 0, mod = 1e9 + 7;
         vector<int> m(26, 0);
         for (char c : s) m[c - 'a']++;
 
         while (t--) {
             int z = m[25];  
             for (int i = 24; i >= 0; --i)m[i+1] = m[i];
-            m[0] = z%mod;
-            m[1] =(m[1]+z)%mod;
-            ans =(ans+z)%mod;
+            m[0]= z%mod;
+            m[1]=(m[1]+z)%mod;
         }
+        for (int i = 0; i <26;i++) ans=(ans+m[i])%mod;
         return ans;
     }
 };
