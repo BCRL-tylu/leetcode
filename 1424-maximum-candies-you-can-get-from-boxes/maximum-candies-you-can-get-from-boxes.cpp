e89bd6aafb289471558345d33489277fe7e77840
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-06-03 16:23:59 +1000

    Time: 6 ms (46.15%) | Memory: 48.1 MB (7.69%) - LeetSync

--- 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/maximum-candies-you-can-get-from-boxes.cpp
@@ -6,43 +6,33 @@ public:
                    vector<int>& initialBoxes) {
         int n = status.size(), ans = 0;
         vector<int> used(n, 0), hold(n, 0);
-
-std::function<void(int)> dfs = [&](int start) -> void {
-    if (used[start]) return;
-
-    stack<int> stk;
-    stk.push(start);
-
-    while (!stk.empty()) {
-        int c = stk.top();
-        stk.pop();
-
-        if (used[c]) continue;
-
-        if (!status[c]) {
-            hold[c] = true;
-            continue;
-        }
-
-        ans += candies[c];
-        used[c] = true;
-
-        for (int k : keys[c]) {
-            status[k] = true; 
-            if (hold[k] && !used[k]) {
-                dfs(k);
+        
+       std::function<void(int)> dfs = [&](int start) ->  void{
+            if (used[start])return;
+            stack<int> stk;
+            stk.push(start);
+            while (!stk.empty()) {
+                int c = stk.top();
+                stk.pop();
+                if (used[c]) continue;
+                if (!status[c]){
+                    hold[c]=1;
+                    continue;
+                }
+                ans += candies[c];
+                used[c] = 1;
+                for (auto k : keys[c]) {
+                    status[k] = 1;
+                    if(hold[k]==1&&!used[k]){
+                        dfs(k);
+                    }
+                }
+                for (auto k : containedBoxes[c]) {
+                    stk.push(k);
+                }
             }
-        }
-
-        // push any boxes contained within c
-        for (int k : containedBoxes[c]) {
-            if (!used[k]) {
-                stk.push(k);
-            }
-        }
-    }
-};
- for (auto start : initialBoxes) {
+        };
+        for (auto start : initialBoxes) {
             dfs(start);
         }
         return ans;
