826fcf59f8742fe5f91f2a98f6e5ffb9892a5c95
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-06-20 11:05:12 +1000

    Time: 0 ms (100.00%) | Memory: 27.1 MB (39.96%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,30 +1,21 @@
-#include <vector>
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        stack<int> st;
-        int water = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            // As long as the current bar is higher than the bar at stack top,
-            // we can trap water over the bar at 'top'
-            while (!st.empty() && height[i] > height[st.top()]) {
-                int top = st.top();
-                st.pop();
-                if (st.empty()) break;  // no left boundary
-
-                int left = st.top();
-                int width = i - left - 1;
-                int bounded_height = min(height[i], height[left]) - height[top];
-                water += width * bounded_height;
-            }
-            st.push(i);
+        vector<int> left(n,0);
+        vector<int> right(n,0);
+        left[0] = height[0];
+        right[n-1] = height[n-1];
+        for(int i=1;i<n;i++){
+            left[i] = max(height[i],left[i-1]);
         }
-        
-        return water;
+        for(int i=n-2;i>=0;i--){
+            right[i] = max(height[i],right[i+1]);
+        }
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            ans += min(left[i],right[i]) - height[i];
+        }
+        return ans;
     }
 };
