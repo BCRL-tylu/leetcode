599cee6672e5942e4a298c1aa43657c8134b7c0b
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-06-23 12:16:37 +1000

    Time: 183 ms (73.68%) | Memory: 7.9 MB (89.47%) - LeetSync

--- 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.cpp
+++ 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.cpp
@@ -1,42 +1,46 @@
 class Solution {
 private:
-    std::string toBaseK(long long x, int k) {
-        assert(k >= 2 && k <= 9);
-        if (x == 0) return "0";
-        std::string s;
-        while (x > 0) {
-            int d = x % k;
-            s.push_back(char('0' + d));
-            x /= k;
-        }
-        std::reverse(s.begin(), s.end());
-        return s;
-    }
-
-    bool isPalindrome(const std::string &s) {
-        return std::equal(s.begin(), s.end(), s.rbegin());
-    }
+    int digit[100];
 
 public:
     long long kMirror(int k, int n) {
-        std::vector<long long> found;
-        for (int len = 1; found.size() < n; ++len) {
-            int half = (len + 1) / 2;
-            int start = (half == 1 ? 1 : int(std::pow(10, half-1)));
-            int end   = int(std::pow(10, half)) - 1;
-            for (int prefix = start;
-                 prefix <= end && found.size() < n;
-                 ++prefix) {
-                std::string s = std::to_string(prefix);
-                std::string t = s.substr(0, len % 2 ? s.size()-1 : s.size());
-                std::reverse(t.begin(), t.end());
-                long long pal = std::stoll(s + t);
-
-                auto bk = toBaseK(pal, k);
-                if (isPalindrome(bk))
-                    found.push_back(pal);
+        auto isPalindrome = [&](long long x) -> bool {
+            int length = -1;
+            while (x) {
+                ++length;
+                digit[length] = x % k;
+                x /= k;
             }
+            for (int i = 0, j = length; i < j; ++i, --j) {
+                if (digit[i] != digit[j]) {
+                    return false;
+                }
+            }
+            return true;
+        };
+
+        int left = 1, count = 0;
+        long long ans = 0;
+        while (count < n) {
+            int right = left * 10;
+            // op = 0 表示枚举奇数长度回文，op = 1 表示枚举偶数长度回文
+            for (int op = 0; op < 2; ++op) {
+                // 枚举 i'
+                for (int i = left; i < right && count < n; ++i) {
+                    long long combined = i;
+                    int x = (op == 0 ? i / 10 : i);
+                    while (x) {
+                        combined = combined * 10 + x % 10;
+                        x /= 10;
+                    }
+                    if (isPalindrome(combined)) {
+                        ++count;
+                        ans += combined;
+                    }
+                }
+            }
+            left = right;
         }
-        return std::accumulate(found.begin(), found.end(), 0LL);
+        return ans;
     }
 };
