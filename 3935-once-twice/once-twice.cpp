785e5fab8f64f2e41ae446e7f4beeeb918d9b470
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-06-24 13:23:15 +1000

    Time: 23 ms (100.00%) | Memory: 99.1 MB (100.00%) - LeetSync

---
+++ 3935-once-twice/once-twice.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> onceTwice(vector<int>& nums) {
+        long long cnt[32]={};
+        for(int v:nums){
+            unsigned u=v;
+            for(int i=0;i<32;i++) cnt[i]+=(u>>i)&1u;
+        }
+        unsigned diff=0;
+        for(int i=0;i<32;i++) if(cnt[i]%3) diff|=1u<<i;
+        unsigned key=diff&-diff;
+        int cnta[32]={},cntb[32]={};
+        for(int v:nums){
+            unsigned u=v;
+            if(u&key){
+                for(int i=0;i<32;i++) cnta[i]+=(u>>i)&1u;
+            } else {
+                for(int i=0;i<32;i++) cntb[i]+=(u>>i)&1u;
+            }
+        }
+        int vala=0,valb=0;
+        for(int i=0;i<32;i++){
+            if(cnta[i]%3) vala|=1u<<i;
+            if(cntb[i]%3) valb|=1u<<i;
+        }
+        int counta=0,countb=0;
+        for(int v:nums){
+            if(v==vala) counta++;
+            else if(v==valb) countb++;
+        }
+        return counta==1?vector<int>{vala,valb}:vector<int>{valb,vala};
+    }
+};
