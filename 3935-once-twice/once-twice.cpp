1962a476232a7101fde9f31557a6fa479a910bbf
BCRL-tylu <143685040+BCRL-tylu@users.noreply.github.com>
2025-06-25 11:51:06 +1000

    Time: 3 ms (0.00%) | Memory: 99.2 MB (0.00%) - LeetSync

--- 3935-once-twice/once-twice.cpp
+++ 3935-once-twice/once-twice.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     vector<int> onceTwice(vector<int>& nums) {
-        int cnt[32]={};
-        for(int v:nums){
-            int u=v;
-            for(int i=0;i<32;i++) cnt[i]+=(u>>i)&1;
-        }
-        int diff=0;
-        for(int i=0;i<32;i++) if(cnt[i]%3) diff|=1<<i;
-        int pivot=diff&-diff;
-        int cnta[32]={},cntb[32]={};
-        for(int v:nums){
-            int u=v;
-            if(u&pivot){
-                for(int i=0;i<32;i++) cnta[i]+=(u>>i)&1;
-            } else {
-                for(int i=0;i<32;i++) cntb[i]+=(u>>i)&1;
+        vector<unsigned int> dp = {~0u, 0, 0};
+        for (unsigned int x : nums) {
+            for (int i = 0, temp = dp[2] & x; i < 3; ++i) {
+                const int mask = x & dp[i];
+                dp[i] ^= mask;
+                dp[i] |= temp;
+                temp = mask;
             }
         }
-        int vala=0,valb=0;
-        for(int i=0;i<32;i++){
-            if(cnta[i]%3) vala|=1<<i;
-            if(cntb[i]%3) valb|=1<<i;
-        }
-        for(int i=0;i<32;i++){
-            if(cnta[i]%3==1){
-                return vector<int>{vala,valb};
-            }else if(cnta[i]%3==2){
-                return vector<int>{valb,vala};
+
+        vector<unsigned int> ddp = {~0u, 0, 0};
+        for (unsigned int x : nums) {
+            if ((x & dp[1]) != dp[1]) continue;
+            if (x & dp[2]) continue;
+            for (int i = 0, temp = ddp[2] & x; i < 3; ++i) {
+                const int mask = x & ddp[i];
+                ddp[i] ^= mask;
+                ddp[i] |= temp;
+                temp = mask;
             }
         }
-        return {-1};
+        return {(int) ddp[1], (int) ((ddp[1] ^ dp[1]) | dp[2])};
+        
     }
 };
